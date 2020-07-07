bf8688653a77b0696b2dd5218d336a56a533a7e2
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-07-07 14:23:07 +0300

    ampcor::dom: updated the subpackage public header

--- lib/ampcor/dom.h
+++ lib/ampcor/dom.h
@@ -1,19 +1,18 @@
-// -*- C++ -*-
-// -*- coding: utf-8 -*-
+// -*- c++ -*-
 //
 // michael a.g. aïvázis <michael.aivazis@para-sim.com>
 // parasim
 // (c) 1998-2020 all rights reserved
-//
 
 // code guard
-#if !defined(ampcor_libampcor_dom_h)
-#define ampcor_libampcor_dom_h
+#if !defined(ampcor_dom_h)
+#define ampcor_dom_h
 
-// support
+
+// load the DOM api
 #include "dom/public.h"
 
-// code guard
-#endif
+
+# endif
 
 // end of file
