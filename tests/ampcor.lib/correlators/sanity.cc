94d2c6b7efb8645540084fe95e7fc407dffb748a
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-07-20 11:43:52 +0300

    ampcor::correlators: primed the test suite

---
+++ tests/ampcor.lib/correlators/sanity.cc
@@ -0,0 +1,19 @@
+// -*- c++ -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// parasim
+// (c) 1998-2020 all rights reserved
+
+
+// get the header
+#include <ampcor/correlators.h>
+
+
+// trivial driver
+int main(int argc, char *argv[]) {
+    // all done
+    return 0;
+}
+
+
+// end of file
