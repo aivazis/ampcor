8d70f3d2ffd63035f759c8a11a28fd1216ce4e98
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-07-21 12:11:46 +0300

    ampcor.correlators: {sequential} got its own timer

--- pkg/ampcor/correlators/Sequential.py
+++ pkg/ampcor/correlators/Sequential.py
@@ -6,6 +6,8 @@
 #
 
 
+# externals
+import journal
 # get the package
 import ampcor
 
@@ -25,6 +27,11 @@ class Sequential:
         """
         Compute the offset map between a pair of {rasters} given a correlation {plan}
         """
+        # make a timer
+        timer = ampcor.executive.newTimer(name="ampcor.sequential")
+        # and a journal channel
+        channel = journal.info("ampcor.sequential.timings")
+
         # unpack the raster
         ref, sec = rasters
         # ask the plan for the number of valid tile pairs
@@ -41,18 +48,31 @@ class Sequential:
 
         # access the bindings; this is guaranteed to succeed
         libampcor = ampcor.ext.libampcor
+        # start the timer
+        timer.reset().start()
         # instantiate my worker
         worker = libampcor.Sequential(pairs=pairs,
                                       ref=chip, sec=window,
                                       refineFactor=refineFactor, refineMargin=refineMargin,
                                       zoomFactor=zoomFactor
                                       )
+        # stop the timer
+        timer.stop()
+        # show me
+        channel.log(f"instantiated the sequential worker: {1e3 * timer.read():.3f} ms")
+
+        # start the timer
+        timer.reset().start()
         # go through the valid (reference,secondary) tile pairs
         for idx, (r,s) in enumerate(plan.pairs):
             # load the reference tile
             worker.addReferenceTile(raster=ref, tid=idx, origin=r.begin, shape=r.shape)
             # load the secondary tile
-            worker.addSecondaryTile(raster=sec, tid=idx, origin=s.begin, shape=s.shape)
+            worker.addReferenceTile(raster=sec, tid=idx, origin=s.begin, shape=s.shape)
+        # stop the timer
+        timer.stop()
+        # show me
+        channel.log(f"transferred the tiles to the coarse arena: {1e3 * timer.read():.3f} ms")
 
         # all done
         return
