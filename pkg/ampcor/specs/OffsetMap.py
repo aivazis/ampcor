42895cf4ddcd56bdc6b1723400f9729c1c0f856b
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-08-03 17:33:40 +0300

    ampcor.specs: added product specifications

---
+++ pkg/ampcor/specs/OffsetMap.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# parasim
+# (c) 1998-2020 all rights reserved
+#
+
+
+# the package
+import ampcor
+
+
+# declaration
+class OffsetMap(ampcor.flow.specification, family="ampcor.products.offsets"):
+    """
+    Access to the data of a file based SLC
+    """
+
+
+    # public data
+    shape = ampcor.properties.tuple(schema=ampcor.properties.int())
+    shape.doc = "the shape of the raster in pixels"
+
+    data = ampcor.properties.path()
+    data.doc = "the path to my binary data"
+
+
+    # framework hooks
+    @classmethod
+    def pyre_default(cls, **kwds):
+        """
+        Provide access to the reference implementation
+        """
+        # invoke the foundry and publish the product
+        return ampcor.products.offsets()
+
+
+
+# end of file
---
+++ pkg/ampcor/specs/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# parasim
+# (c) 1998-2020 all rights reserved
+#
+
+
+# publish the product specs
+from .SLC import SLC as slc
+from .OffsetMap import OffsetMap as offsets
+
+
+# end of file
