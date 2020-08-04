3954b4b950004203e31e1c8fdb4cb717f2d4a582
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-08-04 13:03:50 +0300

    ampcor.cli: {offsets} is now the {ampcor} workflow host; the {info} command now delegates the report formation to the workflow nodes

--- pkg/ampcor/cli/Offsets.py
+++ pkg/ampcor/cli/Offsets.py
@@ -18,21 +18,9 @@ class Offsets(ampcor.shells.command, family="ampcor.cli.offsets"):
     """
 
 
-    # user configurable state
-    # the input data products
-    reference = ampcor.specs.slc()
-    reference.doc = "the reference raster image"
-
-    secondary = ampcor.specs.slc()
-    secondary.doc = "the secondary raster image"
-
-    # the output data product
-    offsetMap = ampcor.specs.offsets()
-    offsetMap.doc = "the offset map from the reference to the secondary raster"
-
-    # the factory
-    correlator = ampcor.specs.correlator()
-    correlator.doc = "the calculator of the offset field"
+    # my workflow
+    flow = ampcor.specs.ampcor()
+    flow.doc = "the ampcor workflow"
 
 
     # behaviors
@@ -41,6 +29,7 @@ class Offsets(ampcor.shells.command, family="ampcor.cli.offsets"):
         """
         Produce an offset map between the {reference} and {secondary} images
         """
+        raise NotImplementedError("NYI!")
         # set up the workflow
         correlator = self.flow()
         # and ask it to do its thing; {plexus} carries the runtime context, so pass it along
@@ -52,9 +41,6 @@ class Offsets(ampcor.shells.command, family="ampcor.cli.offsets"):
         """
         Display the action configuration
         """
-        # configure the workflow
-        self.flow()
-
         # grab a channel
         channel = journal.info("ampcor.info")
         # get things going
@@ -74,11 +60,8 @@ class Offsets(ampcor.shells.command, family="ampcor.cli.offsets"):
         """
         # get shell
         shell = plexus.shell
-        # unpack my arguments
-        reference = self.reference
-        secondary = self.secondary
-        offsets = self.offsetMap
-        correlator = self.correlator
+        # get my flow
+        flow = self.flow
 
         # show the shell configuration
         yield f"{margin}shell: {plexus.shell}"
@@ -86,62 +69,12 @@ class Offsets(ampcor.shells.command, family="ampcor.cli.offsets"):
         yield f"{margin}{indent}tasks: {plexus.shell.tasks} per host"
         yield f"{margin}{indent}gpus:  {plexus.shell.gpus} per task"
 
-        # inputs
-        yield f"{margin}input rasters:"
-
-        # reference raster
-        yield f"{margin}{indent}reference: {reference}"
-        # if i have one
-        if reference:
-            yield f"{margin}{indent*2}data: {reference.data}"
-            yield f"{margin}{indent*2}shape: {reference.shape}"
-            yield f"{margin}{indent*2}pixels: {reference.cells()}"
-            yield f"{margin}{indent*2}footprint: {reference.bytes()} bytes"
-
-        # secondary raster
-        yield f"{margin}{indent}secondary: {secondary}"
-        # if i have one
-        if secondary:
-            yield f"{margin}{indent*2}data: {secondary.data}"
-            yield f"{margin}{indent*2}shape: {secondary.shape}"
-            yield f"{margin}{indent*2}pixels: {secondary.cells()}"
-            yield f"{margin}{indent*2}footprint: {secondary.bytes()} bytes"
-
-        # the output
-        yield f"{margin}output:"
-        yield f"{margin}{indent}offsets: {offsets}"
-        # if i have one
-        if offsets:
-            # show me
-            yield f"{margin}{indent*2}shape: {offsets.shape}"
-
-        # the factory
-        yield from correlator.show(indent=indent, margin=margin)
+        # flow
+        yield f"{margin}flow:"
+        yield from flow.show(margin=margin+indent, indent=indent)
 
         # all done
         return
-
-
-    def flow(self):
-        """
-        Assemble the workflow
-        """
-        # unpack my products
-        reference = self.reference
-        secondary = self.secondary
-        offsets = self.offsetMap
-        # and my factory
-        correlator = self.correlator
-
-        # configure the workflow
-        # set up the inputs
-        correlator.reference = reference
-        correlator.secondary = secondary
-        # attach the output
-        correlator.offsets = offsets
-
-        # and return the factory
-        return correlator
 
 
 # end of file
