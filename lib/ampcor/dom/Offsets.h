6ca87d379cb86f48d8507889840b1c1baa4e4535
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-08-08 12:51:31 +0300

    ampcor.dom: changed the layout of {offsets_t} to a 3d grid with a fixed third shape value to provide fine grained access to the stored info; the read/modify/write cycle of the old layout required an entire record to be read+written to modify even a single entry

--- lib/ampcor/dom/Offsets.h
+++ lib/ampcor/dom/Offsets.h
@@ -30,10 +30,10 @@ public:
     //     the (line,sample) from the reference image
     //     the pixel offsets into the secondary image
     //     the confidence of the map
-    //     a pair of metrics: SNR and covariance
-    using pixel_type = std::tuple<int, int, float, float, float, float>;
+    //     room for metrics: currently, SNR and covariance
+    using pixel_type = float;
     // my layout
-    using layout_type = layout_t<2>;
+    using layout_type = layout_t<3>;
     using layout_const_reference = const layout_type &;
     // size of things
     using size_type = typename layout_type::size_type;
@@ -51,6 +51,10 @@ public:
 
     constexpr auto layout() const -> layout_type;
     constexpr auto shape() const -> layout_type::shape_type;
+
+    // constants
+public:
+    static constexpr auto vars() -> size_type;
 
     // implementation details: data
 private:
