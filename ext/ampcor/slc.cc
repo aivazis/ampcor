db5537dd9546732666ac2d0ed02237d0abce1688
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-08-09 14:05:27 +0300

    ampcor.ext: the {slc_t} product spec now has draft bindings to many of its parts

--- ext/ampcor/slc.cc
+++ ext/ampcor/slc.cc
@@ -28,8 +28,103 @@ namespace ampcor::py {
 void
 ampcor::py::
 slc(py::module &m) {
-    // the SLC interface
-    py::class_<slc_t>(m, "SLC")
+    // type aliases
+    using layout_t = slc_t::layout_type;
+    using index_t = slc_t::layout_type::index_type;
+    using shape_t = slc_t::layout_type::shape_type;
+
+    // declare the product specification class
+    auto pySLC = py::class_<slc_t>(m, "SLC");
+    // embed its layout
+    auto pySLCLayout = py::class_<layout_t>(pySLC, "Layout");
+    // its index
+    auto pySLCIndex = py::class_<index_t>(pySLC, "Index");
+    // and its shape
+    auto pySLCShape = py::class_<shape_t>(pySLCLayout, "Shape");
+
+    // add the SLC layout interface
+    pySLCLayout
+        .def_property_readonly("origin",
+                               // the getter
+                               &layout_t::origin,
+                               // the docstring
+                               "the origin of the SLC layout"
+                               )
+
+        .def_property_readonly("shape",
+                               // the getter
+                               &layout_t::shape,
+                               // the docstring
+                               "the shape of the SLC layout"
+                               )
+
+        // sizes of things: number of pixels
+        .def_property_readonly("cells",
+                               // the getter
+                               &layout_t::cells,
+                               // the docstring
+                               "the number of pixels in the SLC"
+                               )
+        // and number of bytes
+        .def_property_readonly("bytes",
+                               // the getter
+                               [](const layout_t & layout) {
+                                   // easy enough
+                                   return layout.cells() * sizeof(slc_t::pixel_type);
+                               },
+                               // the docstring
+                               "the number of pixels in the SLC"
+                               )
+        // done
+        ;
+
+    // add the SLC index interface
+    pySLCIndex
+        // access to individual ranks
+        .def("__getitem__",
+             // return the value of the requested rank
+             [](const index_t & index, int rank) { return index[rank]; },
+             // signature
+             "rank"_a
+             )
+        // iteration support
+        .def("__iter__",
+             // make an iterator and return it
+             [](const index_t & index) { return py::make_iterator(index.begin(), index.end()); },
+             // make sure the index lives long enough
+             py::keep_alive<0,1>()
+             )
+        // done
+        ;
+
+    // add the SLC shape interface
+    pySLCShape
+        // sizes of things: number of pixels
+        .def_property_readonly("cells",
+                               // the getter
+                               &shape_t::cells,
+                               // the docstring
+                               "the number of pixels in the SLC"
+                               )
+        // access to individual ranks
+        .def("__getitem__",
+             // return the value of the requested rank
+             [](const shape_t & shape, int idx) { return shape[idx]; },
+             // signature
+             "index"_a
+             )
+        // iteration support
+        .def("__iter__",
+             // make an iterator and return it
+             [](const shape_t & shape) { return py::make_iterator(shape.begin(), shape.end()); },
+             // make sure the shape lives long enough
+             py::keep_alive<0,1>()
+             )
+        // done
+        ;
+
+    // add the SLC interface
+    pySLC
         // constructor
         .def(
              // the constructor wrapper
@@ -42,20 +137,55 @@ slc(py::module &m) {
              )
 
         // accessors
+        .def_property_readonly("layout",
+                               // the getter
+                               &slc_t::layout,
+                               // the docstring
+                               "the layout of the SLC raster"
+                               )
+
+        .def_property_readonly("shape",
+                               // the getter
+                               &slc_t::shape,
+                               // the docstring
+                               "the shape of the SLC raster"
+                               )
+
         // sizes of things: number of pixels
         .def_property_readonly("cells",
-                      // the getter
-                      &slc_t::cells,
-                      // the docstring
-                      "the number of pixels in the SLC"
-                      )
+                               // the getter
+                               &slc_t::cells,
+                               // the docstring
+                               "the number of pixels in the SLC"
+                               )
         // sizes of things: memory footprint
         .def_property_readonly("bytes",
-                      // the getter
-                      &slc_t::bytes,
-                      // the docstring
-                      "the amount of memory occupied by this SLC, in bytes"
-                      )
+                               // the getter
+                               &slc_t::bytes,
+                               // the docstring
+                               "the amount of memory occupied by this SLC, in bytes"
+                               )
+        // make a slice
+        .def("slice",
+             //
+             [](const slc_t & slc, py::tuple pyOrigin, py::tuple pyShape) {
+                 // type alises
+                 using index_t = slc_t::layout_type::index_type;
+                 using shape_t = slc_t::layout_type::shape_type;
+
+                 // build the index
+                 index_t idx { pyOrigin[0].cast<int>(), pyOrigin[1].cast<int>() };
+                 // build the shape
+                 shape_t shp { pyShape[0].cast<size_t>(), pyShape[1].cast<size_t>() };
+
+                 // all done
+                 return slc.layout().box(idx, shp);
+             },
+             // the docstring
+             "make a slice at {origin} with the given {shape}",
+             // the signature
+             "origin"_a, "shape"_a
+             )
         // done
         ;
 
