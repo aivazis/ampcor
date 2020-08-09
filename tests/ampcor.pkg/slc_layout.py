6628f4a69459df4f9ef3361ca0f8798d652ca38e
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-08-09 14:09:53 +0300

    ampcor.products: added test cases that exercise getting the {slc} layout and making slices

---
+++ tests/ampcor.pkg/slc_layout.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# parasim
+# (c) 1998-2020 all rights reserved
+#
+
+
+"""
+Sanity check: verify that the {ampcor} package is accessible
+"""
+
+
+def test():
+    # access the {ampcor} package
+    import ampcor
+    # and the journal
+    import journal
+
+    # make a channel
+    channel = journal.debug("ampcor.slc")
+
+    # activate some channels
+    # journal.debug("pyre.memory.direct").activate()
+    # journal.debug("ampcor.slc").activate()
+
+    # create an SLC
+    slc = ampcor.products.newSLC(name="ref")
+
+    # get the layout
+    layout = slc.layout
+
+    # make sure we get the same information from the {layout} as from the spec
+    assert slc.cells() == layout.cells
+    assert slc.bytes() == layout.bytes
+
+    # all done
+    return 0
+
+
+# main
+if __name__ == "__main__":
+    # do...
+    status = test()
+    # share
+    raise SystemExit(status)
+
+
+# end of file
---
+++ tests/ampcor.pkg/slc_slice.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# parasim
+# (c) 1998-2020 all rights reserved
+#
+
+
+"""
+Sanity check: verify that the {ampcor} package is accessible
+"""
+
+
+def test():
+    # access the {ampcor} package
+    import ampcor
+    # and the journal
+    import journal
+
+    # make a channel
+    channel = journal.debug("ampcor.slc")
+
+    # activate some channels
+    # journal.debug("pyre.memory.direct").activate()
+    # journal.debug("ampcor.slc").activate()
+
+    # create an SLC
+    slc = ampcor.products.newSLC(name="ref")
+
+    # pick an origin
+    origin = 0,0
+    # and a shape
+    shape = 128, 128
+    # make a slice
+    slice = slc.slice(origin=origin, shape=shape)
+
+    # check
+    assert tuple(slice.origin) == origin
+    assert tuple(slice.shape) == shape
+
+    # all done
+    return 0
+
+
+# main
+if __name__ == "__main__":
+    # do...
+    status = test()
+    # share
+    raise SystemExit(status)
+
+
+# end of file
