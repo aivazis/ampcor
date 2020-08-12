9d54afdc18a88fe92b8e0af272237b662f87de92
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-08-12 14:38:18 +0300

    ampcor.ext: added {layout_t}, {index_t}, and {shape_t} bindings to the {offsets_t} product spec

--- ext/ampcor/offsets.cc
+++ ext/ampcor/offsets.cc
@@ -13,8 +13,13 @@
 
 
 // type aliases
-using offsets_t = ampcor::dom::offsets_t;
-using pixel_t = offsets_t::pixel_type;
+namespace ampcor::py {
+    using offsets_t = ampcor::dom::offsets_t;
+    using offsets_pixel_t = offsets_t::pixel_type;
+    using offsets_layout_t = offsets_t::layout_type;
+    using offsets_index_t = offsets_t::layout_type::index_type;
+    using offsets_shape_t = offsets_t::layout_type::shape_type;
+}
 
 
 // helpers
@@ -30,18 +35,24 @@ offsets(py::module &m) {
     // the product spec
     py::class_<offsets_t> pyOffsets(m, "Offsets");
     // its pixel type
-    py::class_<pixel_t> pyPixel(pyOffsets, "Pixel");
+    py::class_<offsets_pixel_t> pyPixel(pyOffsets, "Pixel", py::module_local());
+    // embed its layout
+    auto pyOffsetsLayout = py::class_<offsets_layout_t>(pyOffsets, "Layout", py::module_local());
+    // its index
+    auto pyOffsetsIndex = py::class_<offsets_index_t>(pyOffsets, "Index", py::module_local());
+    // and its shape
+    auto pyOffsetsShape = py::class_<offsets_shape_t>(pyOffsetsLayout, "Shape", py::module_local());
 
     // the pixel definition
     pyPixel
         // the reference index
         .def_property("ref",
                       // the getter
-                      [](const pixel_t & pxl) {
+                      [](const offsets_pixel_t & pxl) {
                           return pxl.ref;
                       },
                       // the setter
-                      [](pixel_t & pxl, std::pair<float, float> ref) {
+                      [](offsets_pixel_t & pxl, std::pair<float, float> ref) {
                           pxl.ref = ref;
                       },
                       // the docstring
@@ -50,11 +61,11 @@ offsets(py::module &m) {
         // the shift
         .def_property("delta",
                       // the getter
-                      [](const pixel_t & pxl) {
+                      [](const offsets_pixel_t & pxl) {
                           return pxl.shift;
                       },
                       // the setter
-                      [](pixel_t & pxl, std::pair<float, float> delta) {
+                      [](offsets_pixel_t & pxl, std::pair<float, float> delta) {
                          pxl.shift = delta;
                       },
                       // the docstring
@@ -63,11 +74,11 @@ offsets(py::module &m) {
         // the level of confidence in the mapping
         .def_property("confidence",
                       // the getter
-                      [](const pixel_t & pxl) {
+                      [](const offsets_pixel_t & pxl) {
                           return pxl.confidence;
                       },
                       // the setter
-                      [](pixel_t & pxl, float confidence) {
+                      [](offsets_pixel_t & pxl, float confidence) {
                          pxl.confidence = confidence;
                       },
                       // the docstring
@@ -76,11 +87,11 @@ offsets(py::module &m) {
         // the signal to noise ration
         .def_property("snr",
                       // the getter
-                      [](const pixel_t & pxl) {
+                      [](const offsets_pixel_t & pxl) {
                           return pxl.snr;
                       },
                       // the setter
-                      [](pixel_t & pxl, float snr) {
+                      [](offsets_pixel_t & pxl, float snr) {
                          pxl.snr = snr;
                       },
                       // the docstring
@@ -89,11 +100,11 @@ offsets(py::module &m) {
         // the covariance
         .def_property("covariance",
                       // the getter
-                      [](const pixel_t & pxl) {
+                      [](const offsets_pixel_t & pxl) {
                           return pxl.covariance;
                       },
                       // the setter
-                      [](pixel_t & pxl, float covariance) {
+                      [](offsets_pixel_t & pxl, float covariance) {
                          pxl.covariance = covariance;
                       },
                       // the docstring
@@ -102,7 +113,94 @@ offsets(py::module &m) {
         // done
         ;
 
-    // the offsets interface
+    // add the offset map layout interface
+    pyOffsetsLayout
+        .def_property_readonly("origin",
+                               // the getter
+                               &offsets_layout_t::origin,
+                               // the docstring
+                               "the origin of the offset map layout"
+                               )
+
+        .def_property_readonly("shape",
+                               // the getter
+                               &offsets_layout_t::shape,
+                               // the docstring
+                               "the shape of the offset map layout"
+                               )
+
+        // sizes of things: number of pixels
+        .def_property_readonly("cells",
+                               // the getter
+                               &offsets_layout_t::cells,
+                               // the docstring
+                               "the number of cells in the offsets map"
+                               )
+        // and number of bytes
+        .def_property_readonly("bytes",
+                               // the getter
+                               [](const offsets_layout_t & layout) {
+                                   // easy enough
+                                   return layout.cells() * sizeof(offsets_t::pixel_type);
+                               },
+                               // the docstring
+                               "the memory footprint of the offsets map, in bytes"
+                               )
+        // done
+        ;
+
+    // add the Offsets index interface
+    pyOffsetsIndex
+        // access to individual ranks
+        .def("__getitem__",
+             // return the value of the requested rank
+             [](const offsets_index_t & index, int rank) { return index[rank]; },
+             // signature
+             "rank"_a
+             )
+        // iteration support
+        .def("__iter__",
+             // make an iterator and return it
+             [](const offsets_index_t & index) {
+                 return py::make_iterator(index.begin(), index.end());
+             },
+             // make sure the index lives long enough
+             py::keep_alive<0,1>()
+             )
+        // done
+        ;
+
+    // add the shape interface
+    pyOffsetsShape
+        // sizes of things: number of pixels
+        .def_property_readonly("cells",
+                               // the getter
+                               &offsets_shape_t::cells,
+                               // the docstring
+                               "the number of pixels in the offset map"
+                               )
+        // access to individual ranks
+        .def("__getitem__",
+             // return the value of the requested rank
+             [](const offsets_shape_t & shape, int idx) {
+                 return shape[idx];
+             },
+             // signature
+             "index"_a
+             )
+        // iteration support
+        .def("__iter__",
+             // make an iterator and return it
+             [](const offsets_shape_t & shape) {
+                 return py::make_iterator(shape.begin(), shape.end());
+             },
+             // make sure the shape lives long enough
+             py::keep_alive<0,1>()
+             )
+        // done
+        ;
+
+    // the offset map interface
     pyOffsets
         // constructor
         .def(
