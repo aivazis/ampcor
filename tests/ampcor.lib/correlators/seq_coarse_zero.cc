30d9dc83c8df3b52b1886bed4a9c0b82a52b3ef9
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-08-17 16:19:28 +0300

    ampcor::correlators: the {sequnetial_t} sanity test case now uses the new {arena_t} intermediate product to specify the worker scratch space

--- tests/ampcor.lib/correlators/seq_coarse_zero.cc
+++ tests/ampcor.lib/correlators/seq_coarse_zero.cc
@@ -15,8 +15,7 @@ using namespace std::complex_literals;
 
 
 // type aliases
-using slc_t = ampcor::dom::slc_t;
-using seq_t = ampcor::correlators::sequential_t<slc_t>;
+using seq_t = ampcor::correlators::sequential_t<>;
 
 
 // make a sequential worker and add the reference tiles to its arena
@@ -27,42 +26,28 @@ int main(int argc, char *argv[]) {
     // make a channel
     pyre::journal::debug_t channel("ampcor.correlators.seq");
 
-    // the base dimension
-    size_t dim = 8;
+    // the number of pairs
+    auto pairs = 4;
+    // the base tile dimension
+    auto dim = 8;
 
-    // the shape of the reference raster
-    slc_t::shape_type rasterShape { dim, dim };
-    // the product spec
-    slc_t::spec_type spec { rasterShape };
-    // open the sample reference raster in read-only mode
-    slc_t slc { spec, "slc_ref.dat" };
-
-    // the shape of the reference tiles
-    slc_t::shape_type refShape { dim/4, dim/4 };
-    // and their layout: 0-based, row major
-    slc_t::layout_type refLayout { refShape };
-
-    // the shape of the secondary tiles, which includes their margin
-    slc_t::shape_type secShape { dim/2, dim/2 };
-    // the origin: chosen such that the reference tile shape is at {0,0}
-    slc_t::index_type secOrigin { -dim/8, -dim/8 };
+    // the shape of the reference tile arena
+    seq_t::arena_shape_type refShape { pairs, dim/4, dim/4 };
     // their layout
-    slc_t::layout_type secLayout { secShape, secOrigin };
+    seq_t::arena_layout_type refLayout { refShape };
+
+    // the shape of the secondary tile arena
+    seq_t::arena_shape_type secShape { pairs, dim/2, dim/2 };
+    // the origin: chosen such that the reference tile shape is at {0,0}
+    seq_t::arena_index_type secOrigin { 0, -dim/8, -dim/8 };
+    // their layout
+    seq_t::arena_layout_type secLayout { secShape, secOrigin };
 
     // make a sequential worker
-    seq_t seq(4, refLayout, secLayout, 1, 0, 1);
+    seq_t seq(pairs, refLayout, secLayout, 1, 0, 1);
 
-    // pick a value
-    slc_t::spec_type::value_type v = 42;
-    // zero out its coarse arena
-    seq.fillCoarseArena(v);
-
-    // go through every cell
-    for (auto cursor = seq.coarseArena();
-         cursor != seq.coarseArena()+seq.coarseArenaCells(); ++cursor) {
-        // make sure it contains what we expect
-        assert(( *cursor == v ));
-    }
+    // all we know is the number of pairs
+    assert(( seq.pairs() == pairs ));
 
     // all done
     return 0;
