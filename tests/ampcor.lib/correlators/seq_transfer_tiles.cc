e52c4c9cbdd925774e9b51c092d9f27f9691bff0
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-08-17 16:29:13 +0300

    ampcor::correlators: reworked the {sequential_t} test case that exercises the reference and secondary tile transfer

--- tests/ampcor.lib/correlators/seq_transfer_tiles.cc
+++ tests/ampcor.lib/correlators/seq_transfer_tiles.cc
@@ -15,116 +15,65 @@ using namespace std::complex_literals;
 
 
 // type aliases
-using slc_t = ampcor::dom::slc_t;
-using seq_t = ampcor::correlators::sequential_t<slc_t>;
+using slc_const_raster_t = ampcor::dom::slc_const_raster_t;
+using offsets_raster_t = ampcor::dom::offsets_raster_t;
+using seq_t = ampcor::correlators::sequential_t<slc_const_raster_t, offsets_raster_t>;
 
 
-// make a sequential worker and add the reference tiles to its arena
+// make a sequential worker and add tiles to its arena
+// - the tiles come from the output of the {slc_ref} and {slc_sec} test cases
+// - the reference tiles are (dim/4, dim/4) at the center of (dim/2,dim/2) blocks
+// - the secondary tiles are (dim/2,dim/2) and contain the reference tiles at their bottom
+//   right hand corner
+// run them with activated journal channels to see what they look like
 int main(int argc, char *argv[]) {
     // initialize the journal
     pyre::journal::init(argc, argv);
-    pyre::journal::application("seq_ref");
+    pyre::journal::application("seq_transfer_tiles");
     // make a channel
     pyre::journal::debug_t channel("ampcor.correlators.seq");
 
     // the base dimension
-    size_t dim = 8;
-
-    // the shape of the reference raster
-    slc_t::shape_type rasterShape { dim, dim };
-    // the product spec
-    slc_t::spec_type spec { rasterShape };
-    // open the sample reference raster in read-only mode
-    slc_t slc { spec, "slc_ref.dat" };
-
-    // show me the raster
-    channel << "slc:" << pyre::journal::newline;
-    for (size_t i=0; i<rasterShape[0]; ++i) {
-        for (size_t j=0; j<rasterShape[1]; ++j) {
-            channel << "  " << std::setw(2) << slc[{i,j}];
-        }
-        channel << pyre::journal::newline;
-    }
-    channel << pyre::journal::endl;
-
+    auto dim = 8;
+    // the number of tile pairs
+    auto pairs = 4;
 
     // the shape of the reference tiles
-    slc_t::shape_type refShape { dim/4, dim/4 };
+    seq_t::arena_shape_type refArenaShape { pairs, dim/4, dim/4 };
     // and their layout: 0-based, row major
-    slc_t::layout_type refLayout { refShape };
+    seq_t::arena_layout_type refArenaLayout { refArenaShape };
 
     // the shape of the secondary tiles, which includes their margin
-    slc_t::shape_type secShape { dim/2, dim/2 };
-    // the origin: chosen such that the reference tile shape is at {0,0}
-    slc_t::index_type secOrigin { -dim/8, -dim/8 };
-    // their layout
-    slc_t::layout_type secLayout { secShape, secOrigin };
+    seq_t::arena_shape_type secArenaShape { pairs, dim/2, dim/2 };
+    // the origin: chosen such that a zero shift corresponds to the tile with maximum
+    // correlation having origin at (0,0)
+    seq_t::arena_index_type secArenaOrigin { 0, -dim/8, -dim/8 };
+    // the secondary tile layout
+    seq_t::arena_layout_type secArenaLayout { secArenaShape, secArenaOrigin };
 
     // make a sequential worker with 4 pairs of tiles, trivial refinement and zoom
-    seq_t seq(4, refLayout, secLayout, 1, 0, 1);
+    seq_t seq(pairs, refArenaLayout, secArenaLayout, 1, 0, 1);
 
-    // pick a value
-    slc_t::spec_type::value_type value = 42;
-    // zero out its coarse arena
-    seq.fillCoarseArena(value);
+    // specify and open the rasters
+    // shape
+    slc_const_raster_t::shape_type rasterShape { dim, dim };
+    // product spec
+    slc_const_raster_t::spec_type spec { slc_const_raster_t::layout_type(rasterShape) };
+    // open the sample reference raster in read-only mode
+    slc_const_raster_t refRaster { spec, "slc_ref.dat" };
+    // repeat for the secondary raster
+    slc_const_raster_t secRaster { spec, "slc_sec.dat" };
 
-    // pick a spot somewhere in the middle of the reference raster: the {1,0} tile
-    slc_t::index_type i_10 { dim/2 + dim/8, dim/8 };
-    // build the tile
-    auto t_10 = slc.tile(i_10, refShape);
-
-    channel << "t_10:" << pyre::journal::newline;
-    for (auto i : t_10.layout()) {
-        channel << "  t_10[" << i << "] = " << t_10[i] << pyre::journal::newline;
-    }
-    channel << pyre::journal::endl;
-
-    // add it to the arena
-    seq.addReferenceTile(2, t_10);
-
-    // verify
-    // get the arena
-    auto arena = seq.coarseArena();
-    // its memory footprint
-    auto bytes = seq.coarseArenaBytes();
-    // and its stride to skip over one (ref,sec) pair
-    auto stride = seq.coarseArenaStride();
-
-    // show me
-    channel
-        << "arena: " << bytes << " bytes at " << arena
-        << pyre::journal::endl;
-
-    // the first ref/sec pair should be untouched
-    for (auto c = arena; c != arena+stride; ++c) {
-        // i.e. equal to the initialization value
-        assert(( *c == value ));
-    }
-    // ditto for the second ref/sec pair
-    for (auto c = arena+stride; c != arena+2*stride; ++c) {
-        // i.e. equal to the initialization value
-        assert(( *c == value ));
-    }
-
-    // the third pair has our ref tile; set up a cursor that looks in the arena
-    auto cur = arena + 2*stride;
-    // go through the tile values in packing order
-    for (auto pixel : t_10) {
-        // verify that the value stored in the arena is the magnitude of the tile value
-        assert(( *cur == std::abs(pixel) ));
-        // and grab the next one
-        ++cur;
-    }
-    // the sec tile spot should contain
-    for (auto c = arena+2*stride+t_10.cells(); c != arena+3*stride; ++c) {
-        // the initialization value
-        assert(( *c == value ));
-    }
-
-    // the fourth pair is untouched
-    for (auto c = arena+3*stride; c != arena+4*stride; ++c) {
-        // i.e. equal to the initialization value
-        assert(( *c == value ));
+    // we have four pairs of tiles to transfer
+    for (auto i : {0,1}) {
+        for (auto j : {0,1}) {
+            // form the collation order
+            auto tid = 2*i + j;
+            // add the tiles
+            seq.addTilePair(tid, tid,
+                            refRaster.tile({i*dim/2 + dim/8, j*dim/2 + dim/8}, {dim/4,dim/4}),
+                            secRaster.tile({i*dim/2,j*dim/2}, {dim/2,dim/2}));
+        }
     }
 
     // all done
