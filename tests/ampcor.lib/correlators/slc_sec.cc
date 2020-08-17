3b29bfa9296524f0019df18b602a77f392536b41
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-08-17 16:17:54 +0300

    ampcor::correlators: the secondary reference slc is now generated by pushing the reference slc data to the corners

--- tests/ampcor.lib/correlators/slc_sec.cc
+++ tests/ampcor.lib/correlators/slc_sec.cc
@@ -9,29 +9,29 @@
 #include <cassert>
 // get the header
 #include <ampcor/dom.h>
-// access the complex literals
-using namespace std::complex_literals;
 
 
 // type aliases
-using slc_t = ampcor::dom::slc_t;
-using spec_t = slc_t::spec_type;
+using slc_raster_t = ampcor::dom::slc_raster_t;
+using slc_const_raster_t = ampcor::dom::slc_const_raster_t;
+using spec_t = slc_raster_t::spec_type;
 using pixel_t = spec_t::pixel_type;
 using value_t = spec_t::value_type;
-using shape_t = slc_t::shape_type;
-using index_t = slc_t::index_type;
+using layout_t = slc_raster_t::layout_type;
+using shape_t = slc_raster_t::shape_type;
+using index_t = slc_raster_t::index_type;
 
 
-// create the secondary SLC raster:
+// the secondary SLC raster, in block form:
+//   X X . . . . X X
+//   X X . . . . X X
 //   . . . . . . . .
 //   . . . . . . . .
-//   . . x x . . x x
-//   . . x x . . x x
 //   . . . . . . . .
 //   . . . . . . . .
-//   . . x x . . x x
-//   . . x x . . x x
-//
+//   X X . . . . x x
+//   X X . . . . x x
+// the data come from reading the reference slc
 int main(int argc, char *argv[]) {
     // initialize the journal
     pyre::journal::init(argc, argv);
@@ -39,42 +39,39 @@ int main(int argc, char *argv[]) {
     // make a channel
     pyre::journal::debug_t channel("ampcor.correlators.slc");
 
-    // the name of the product
-    std::string name = "slc_sec.dat";
-    // the base dimension
-    size_t dim = 8;
-    // make a shape
-    shape_t shape { dim, dim };
-    // build the product specification
-    spec_t spec { shape };
+    // the names of the products
+    std::string refName = "slc_ref.dat";
+    std::string secName = "slc_sec.dat";
 
+    // the base dimension
+    auto dim = 8;
+    // make a shape
+    shape_t rasterShape { dim, dim };
+    // build the product specification
+    spec_t spec { layout_t(rasterShape) };
+
+    // get the reference raster
+    slc_const_raster_t ref { spec, refName };
     // make the product
-    slc_t slc { spec, name, spec.cells() };
+    slc_raster_t sec { spec, secName, spec.cells() };
 
     // form a 2x2 grid of tiles
-    for (size_t i : {0,1}) {
-        for (size_t j : {0,1}) {
-            // form the base of the tile
-            index_t base { i*dim/2 + dim/4, j*dim/2 + dim/4 };
-            // form the shape of the tile
-            shape_t shp { dim/4, dim/4 };
-            // make the tile
-            auto tile = slc.tile(base, shp);
-            // now, loop over each tile index space
-            for (const auto & idx : tile.layout()) {
-                // form the value
-                pixel_t value = static_cast<value_t>(idx[0]) + static_cast<value_t>(idx[1])*1if;
-                // and set the corresponding cell
-                slc[idx] = value;
-            }
+    for (auto i : {0,1}) {
+        for (auto j : {0,1}) {
+            // identify the reference data
+            auto src = ref.box({i*dim/2+dim/8, j*dim/2+dim/8}, {dim/4, dim/4});
+            // identify the destination
+            auto dst = sec.box({3*dim*i/4, 3*dim*j/4}, {dim/4, dim/4});
+            // copy the data
+            std::copy(src.begin(), src.end(), dst.begin());
         }
     }
 
     // show me
-    channel << "base:" << pyre::journal::newline;
-    for (size_t i=0; i<shape[0]; ++i) {
-        for (size_t j=0; j<shape[1]; ++j) {
-            channel << "  " << std::setw(2) << slc[{i,j}];
+    channel << "secondary:" << pyre::journal::newline;
+    for (auto i=0; i<rasterShape[0]; ++i) {
+        for (auto j=0; j<rasterShape[1]; ++j) {
+            channel << "  " << std::setw(7) << sec[{i,j}];
         }
         channel << pyre::journal::newline;
     }
