5b67e95b1c4b79da091262f004fda73c259670c0
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-08-20 15:44:39 +0300

    ampcor::correlators: the SAT visualizer now prints the secondary arena, as well as a comparison of the sums of the values of all possible placements of a chip within the secondary tiles

--- tests/ampcor.lib/correlators/coarse_sat.cc
+++ tests/ampcor.lib/correlators/coarse_sat.cc
@@ -15,8 +15,9 @@
 using arena_const_raster_t = ampcor::dom::arena_const_raster_t;
 using spec_t = arena_const_raster_t::spec_type;
 using pixel_t = spec_t::pixel_type;
-using shape_t = arena_const_raster_t::shape_type;
 using index_t = arena_const_raster_t::index_type;
+using shape_t = arena_const_raster_t::shape_type;
+using layout_t = arena_const_raster_t::layout_type;
 
 
 // load the product that has the coarse arena with the reference tiles and dump its contents
@@ -27,32 +28,43 @@ int main(int argc, char *argv[]) {
     // make a channel
     pyre::journal::debug_t channel("ampcor.correlators.arena");
 
-    // the name of the product
-    std::string name = "coarse_sat.dat";
     // the number of pairs
     auto pairs = 4;
     // the base dimension
     auto dim = 8;
+    // a useful index shift
+    index_t deltaUL = { 0, -1, -1 };
 
-    // the origin
-    index_t origin { 0, 0, 0 };
-    // make a shape
-    shape_t shape { pairs, dim/2, dim/2 };
-    // build the product specification
-    spec_t spec { spec_t::layout_type(shape, origin) };
+    // the name of the product with the secondary arena
+    std::string arenaName = "coarse_sec.dat";
+    // the name of the product with the SAT table
+    std::string satName = "coarse_sat.dat";
 
-    // build the product
-    arena_const_raster_t arena { spec, name };
+    // arena: the origin
+    index_t arenaOrigin { 0, -dim/8, -dim/8 };
+    // the shape
+    shape_t arenaShape { pairs, dim/2, dim/2 };
+    // the product specification
+    spec_t arenaSpec { spec_t::layout_type(arenaShape, arenaOrigin) };
 
-    // show me
-    channel << "coarse reference arena:" << pyre::journal::newline;
+    // SAT: the origin
+    index_t satOrigin = arenaOrigin + deltaUL;
+    // the shape
+    shape_t satShape { pairs, dim/2+1, dim/2+1 };
+    // the product specification
+    spec_t satSpec { spec_t::layout_type(satShape, satOrigin) };
+
+    // build the products
+    arena_const_raster_t arena { arenaSpec, arenaName };
+    arena_const_raster_t sat { satSpec, satName };
+
     // go through the tiles
-    for (auto tid = origin[0]; tid < origin[0] + shape[0]; ++tid) {
+    for (auto tid = arenaOrigin[0]; tid < arenaOrigin[0] + arenaShape[0]; ++tid) {
         // show me the tile number
         channel << "tile " << tid << pyre::journal::newline;
         // and the contents
-        for (auto i = origin[1]; i < origin[1] + shape[1]; ++i) {
-            for (auto j = origin[2]; j < origin[2] + shape[2]; ++j) {
+        for (auto i = arenaOrigin[1]; i < arenaOrigin[1] + arenaShape[1]; ++i) {
+            for (auto j = arenaOrigin[2]; j < arenaOrigin[2] + arenaShape[2]; ++j) {
                 // form the index
                 index_t idx { tid, i, j };
                 // show me
@@ -60,9 +72,79 @@ int main(int argc, char *argv[]) {
             }
             channel << pyre::journal::newline;
         }
+
+        // SAT
+        channel << "sum area table:" << pyre::journal::newline;
+        // the contents
+        for (auto i = satOrigin[1]; i < satOrigin[1] + satShape[1]; ++i) {
+            for (auto j = satOrigin[2]; j < satOrigin[2] + satShape[2]; ++j) {
+                // form the index
+                index_t idx { tid, i, j };
+                // show me
+                channel << "  " << std::setw(7) << sat[idx];
+            }
+            channel << pyre::journal::newline;
+        }
     }
     // flush
     channel << pyre::journal::endl(__HERE__);
+
+    // verify: slide chips around the arena and verify that the sum of its elements can be
+    // obtained by using the sat table
+    // make a shape that describes our sliding chip
+    shape_t chipShape { 1, dim/4, dim/4 };
+
+    // for each index that describes the placement of the chip with the secondary tile, we need
+    // the four indices in the SAT that are used in the computation of the sum
+    // project the chip shape on the tile axes
+    index_t chip_1 = { 0, chipShape[1], 0 };
+    index_t chip_2 = { 0, 0, chipShape[2] };
+    // compute them as shift relative to the chip origin
+    index_t deltaUR = deltaUL + chip_2;
+    index_t deltaLL = deltaUL + chip_1;
+    index_t deltaLR = deltaUL + chip_1 + chip_2;
+
+    // we do some floating point comparisons, so...
+    pixel_t zero = 0;
+
+    // go through all the tiles
+    for (auto tid = arenaOrigin[0]; tid < arenaOrigin[0] + arenaShape[0]; ++tid) {
+        // build a layout that will give me all the possible placements of a chip inside the
+        // arena tile
+        channel << "tile #" << tid << pyre::journal::newline;
+        // the origin
+        index_t plOrigin { tid, arenaOrigin[1], arenaOrigin[2] };
+        // the shape
+        shape_t plShape { tid, arenaShape[1]-chipShape[1]+1, arenaShape[2]-chipShape[2]+1 };
+        // the layout
+        layout_t pl { plShape , plOrigin };
+
+        // go through all placements
+        for (auto chipOrigin : pl ) {
+            // form the tile
+            auto tile = arena.box(chipOrigin, chipShape);
+            // compute the sum by visiting the tile slots
+            auto expected = std::accumulate(tile.begin(), tile.end(), zero);
+            // compute the sum by indexing the SAT
+            auto actual =
+                sat[chipOrigin+deltaUL] + sat[chipOrigin+deltaLR]
+                - sat[chipOrigin+deltaUR] - sat[chipOrigin+deltaLL];
+
+            // show me; this is a good example of the need for level of detail in channel output
+            channel
+                << chipOrigin << ": " << pyre::journal::newline
+                << "  expected = " << expected << pyre::journal::newline
+                << "  actual = " << actual
+                << " = " << sat[chipOrigin+deltaUL]
+                << " + " << sat[chipOrigin+deltaLR]
+                << " - " << sat[chipOrigin+deltaUR]
+                << " - " << sat[chipOrigin+deltaLL]
+                << pyre::journal::newline;
+        }
+
+        // flush
+        channel << pyre::journal::endl(__HERE__);
+    }
 
     // all done
     return 0;
