f33555a78509a6ec8b8114516907c6bc7240c063
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-08-30 16:20:54 +0300

    ampcor::correlators: prepping {sequential_t} to be useful as the workhorse of the parallel implementation; it's mostly disabled, for now, as this exercise is about the flow of information

--- lib/ampcor/correlators/Sequential.h
+++ lib/ampcor/correlators/Sequential.h
@@ -29,9 +29,19 @@ public:
     // the slc pixel type
     using slc_pixel_type = typename slc_spec::pixel_type;
 
-    // my constructor depends on the layout of the incoming tiles
+    // my constructor and implementation depend on the layout of input tiles
     using slc_layout_type = typename slc_spec::layout_type;
+    using slc_shape_type = typename slc_layout_type::shape_type;
     using slc_layout_const_reference = const slc_layout_type &;
+    using slc_shape_const_reference = const slc_shape_type &;
+
+    // the specification of the portion of the work assigned to me uses output tiles`
+    using offsets_layout_type = typename offsets_spec::layout_type;
+    using offsets_shape_type = typename offsets_layout_type::shape_type;
+    using offsets_index_type = typename offsets_layout_type::index_type;
+    using offsets_layout_const_reference = const offsets_layout_type &;
+    using offsets_shape_const_reference = const offsets_shape_type &;
+    using offsets_index_const_reference = const offsets_index_type &;
 
     // my record of the original pair collation order
     using pid_grid = int *;
@@ -57,36 +67,32 @@ public:
     // miscellaneous
     using string_type = string_t;
 
-    // metamethods
+    // non-trivial metamethods
 public:
-    // destructor
-    inline ~Sequential();
-
     // constructor
     inline
-    Sequential(slc_const_reference ref, slc_const_reference sec, offsets_reference map,
-               arena_layout_const_reference refLayout, arena_layout_const_reference secLayout,
-               int refineFactor, int refineMargin,
-               int zoomFactor);
-
-    // accessors
-public:
-    inline auto pairs() const -> int;
+    Sequential(int rank,
+               slc_const_reference ref, slc_const_reference sec, offsets_reference map,
+               slc_shape_const_reference refShape, slc_shape_const_reference secShape,
+               int refineFactor, int refineMargin, int zoomFactor);
 
     // interface
 public:
+#if MGA
     // transfer, detect, and store a pair of tiles and record the id of this pairing; note that
     // {tid} tracks the pairings that were assigned to me, while {pid} remembers the collation
     // number of this pairing in the original plan, which may have involved invalid tiles so
     // there may be gaps
     inline void addTilePair(int tid, int pid,
                             slc_const_reference ref, slc_const_reference sec);
+#endif
 
     // execute the correlation plan and adjust the offset map
-    auto adjust();
+    auto adjust(offsets_index_const_reference, offsets_shape_const_reference);
 
     // implementation details: methods
 public:
+#if MGA
     // reduce the tiles in {arena} to zero mean, and compute their variances
     auto _referenceStatistics(arena_reference) -> vector_pointer;
 
@@ -103,28 +109,31 @@ public:
                     const_arena_const_reference, const_arena_const_reference) -> const_arena_type;
     // compute and store the locations of the maxima of the correlation surface
     auto _maxcor(const_arena_const_reference, offsets_reference);
+#endif
 
     // implementation details: data
 private:
+    // my rank; useful in parallel mode
+    int _rank;
     // the input rasters
     slc_const_reference _ref;
     slc_const_reference _sec;
     offsets_reference _map;
-    // my workload
-    const int _pairs;
+    // the shapes of tiles
+    slc_shape_const_reference _refShape;
+    slc_shape_const_reference _secShape;
     // the correlation surface refinement parameters
     const int _refineFactor;
     const int _refineMargin;
     const int _zoomFactor;
 
-    // scratch space
-    pid_grid _pids;
-    // the grid of reference tiles
-    arena_type _refCoarse;
-    arena_type _secCoarse;
+
+    // default metamethods
+public:
+    ~Sequential() = default;
 
     // disabled metamethods
-public:
+private:
     // constructors
     Sequential(const Sequential &) = delete;
     Sequential(Sequential &&) = delete;
