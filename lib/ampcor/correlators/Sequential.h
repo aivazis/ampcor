cef45c4c61c98196aecae1ad0414ca17d13ca9ac
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-09-02 08:14:24 +0300

    ampcor::correlators: reenabled the computation of the correlation matrix in {sequential_t}

--- lib/ampcor/correlators/Sequential.h
+++ lib/ampcor/correlators/Sequential.h
@@ -103,13 +103,13 @@ public:
     auto _secondaryStatistics(string_type,
                               arena_layout_const_reference, arena_layout_const_reference,
                               const_arena_const_reference) -> const_arena_type;
-
-#if MGA
-public:
     // compute the correlation surface
     auto _correlate(string_type,
                     const_arena_const_reference, vector_pointer,
                     const_arena_const_reference, const_arena_const_reference) -> const_arena_type;
+
+#if MGA
+public:
     // compute and store the locations of the maxima of the correlation surface
     auto _maxcor(const_arena_const_reference, offsets_reference);
 #endif
