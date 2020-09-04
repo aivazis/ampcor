25b971ca266169e2fc8761d826760b5a7d0fdc55
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-09-04 19:10:16 +0300

    ampcor::correlators: {sequential_t} now checks whether it is supposed to zoom the correlation surface, and if not, it saves the current {offsets_t} and bails

--- lib/ampcor/correlators/Sequential.h
+++ lib/ampcor/correlators/Sequential.h
@@ -135,7 +135,7 @@ public:
                     const_arena_const_reference, vector_pointer,
                     const_arena_const_reference, const_arena_const_reference) -> const_arena_type;
     // compute and store the locations of the maxima of the correlation surface
-    auto _maxcor(offsets_reference, const_arena_const_reference, int=1) -> void;
+    auto _maxcor(const_arena_const_reference, int) -> void;
 
     // build complex arenas for storing tiles for deramping and refininment
     auto _createComplexArena(string_type, int,
