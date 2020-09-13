37105fad874dcbbecb5caee1b8af3c94d947c8e3
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-09-13 16:55:05 +0300

    ampcor.shells: the plexus is now ready for the {web} shell

--- pkg/ampcor/shells/Ampcor.py
+++ pkg/ampcor/shells/Ampcor.py
@@ -45,4 +45,76 @@ class Ampcor(pyre.plexus, family='ampcor.shells.plexus'):
         return super().pyre_interactiveSessionContext(context=context)
 
 
+    # virtual filesystem configuration
+    def pyre_mountApplicationFolders(self, pfs, prefix, **kwds):
+        """
+        Explore the application installation folders and construct my private filespace
+        """
+        # chain up
+        pfs = super().pyre_mountApplicationFolders(pfs=pfs, prefix=prefix, **kwds)
+        # get my namespace
+        namespace = self.pyre_namespace
+
+        #
+        # this is early times, so {prefix} may not be explored; tread carefully
+        #
+
+        # gingerly, look for the web document root, but avoid expanding parts of the local
+        # filesystem that we don't care about and getting trapped in deep directory structures
+        # start at the top
+        docroot = prefix
+        # and descend where we think the main static asset is
+        for name in ["etc", namespace, "ux"]:
+            # fill the contents of the current node
+            docroot.discover(levels=1)
+            # attempt to
+            try:
+                # descend to the next level
+                docroot = docroot[name]
+            # if not there
+            except prefix.NotFoundError:
+                # grab a channel
+                channel = self.warning
+                # complain
+                channel.line(f"while looking for UX support:")
+                channel.line(f"directory '{docroot.uri}/{name}' not found")
+                channel.line(f"disabling the web shell")
+                channel.log()
+                # mark the UX as unavailable
+                self._ux = None
+                # and bail
+                break
+        # if all goes well and we reach the intended folder without errors
+        else:
+            # expand the directory structure below my document root and mount it
+            pfs['ux'] = docroot.discover()
+            # get my dispatcher
+            from .UX import UX
+            # instantiate and attach
+            self._ux = UX(docroot=docroot)
+
+        # all done
+        return pfs
+
+
+    # main entry point for the web shell
+    def pyre_respond(self, server, request):
+        """
+        Fulfill an HTTP request
+        """
+        # get my dispatcher
+        ux = self._ux
+        # if i don't have one, there is something wrong with my installation
+        if ux is None:
+            # so everything is an error
+            return server.responses.NotFoundError(server=server)
+
+        # otherwise, ask the dispatcher to do its thing
+        return ux.dispatch(plexus=self, server=server, request=request)
+
+
+    # private data
+    _ux = None # the UX manager
+
+
 # end of file
