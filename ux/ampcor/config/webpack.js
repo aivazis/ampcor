9788dddfac82c9265c9d5339243182686f5190e0
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-09-18 11:05:50 +0300

    ampcor.ux: added configuration files for the management and bundling of external dependencies

---
+++ ux/ampcor/config/webpack.js
@@ -0,0 +1,61 @@
+// -*- javascript -*-
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// parasim
+// (c) 1998-2020 all rights reserved
+//
+
+// external dependencies
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// local geography
+const rootDir = __dirname
+const sourceDir = path.join(rootDir, 'react')
+const buildDir = path.join(rootDir, 'build')
+
+
+// the configuration
+module.exports = {
+    // the main entry point
+    entry: {
+        ampcor: path.join(sourceDir, "ampcor.js"),
+    },
+
+    // the build product
+    output: {
+        path: buildDir,
+        filename: '[name].js',
+    },
+
+    // loader rules
+    module: {
+        rules: [
+            {   // jsx
+                test: /\.jsx?$/,
+                loader: 'babel-loader',
+                include: [ sourceDir ],
+            }
+        ]
+    },
+
+    // locations of files
+    resolve: {
+        modules: [sourceDir, "node_modules"],
+        extensions: ['.js', '.jsx'],
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'ampcor.html',
+            inject: 'body',
+            filename: path.join(buildDir, 'ampcor.html')
+        }),
+    ],
+
+    devtool: "inline-source-map",
+
+}
+
+
+// end of file
