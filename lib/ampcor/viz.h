bbc9be82d857d3c25d0825b408579483547ff1de
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-09-21 18:47:55 +0300

    ampcor::viz: added the top-level public header file

---
+++ lib/ampcor/viz.h
@@ -0,0 +1,18 @@
+// -*- c++ -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// parasim
+// (c) 1998-2020 all rights reserved
+
+// code guard
+#if !defined(ampcor_viz_h)
+#define ampcor_viz_h
+
+
+// load the DOM api
+#include "viz/public.h"
+
+
+# endif
+
+// end of file
