2d152ba67f1f3932bc712276ccea4f2c0299cb92
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-09-27 14:53:24 +0300

    ampcor.ux: {safari} stops lying about supporting lazy loading once the {experimental feature} is turned off; hence, the native support plugin from {lazysizes} is now enabled

--- ux/ampcor/client/ampcor.js
+++ ux/ampcor/client/ampcor.js
@@ -9,17 +9,13 @@
 // externals
 // support for image lazy loading
 import lazysizes from 'lazysizes'
-// unfortunately, the native loading plugin doesn't work on safari, because it lies
-// import 'lazysizes/plugins/native-loading/ls.native-loading'
+// use native lazy loading whenever possible
+import 'lazysizes/plugins/native-loading/ls.native-loading'
 
 // the component framework
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-
-if ('loading' in HTMLImageElement.prototype) {
-    console.log("the browser claims to support lazy loading")
-}
 
 // my root view
 import { Layout } from './views'
