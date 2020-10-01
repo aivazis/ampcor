803e0aaf145612ea90845ddccc05bacc18571804
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-10-01 14:02:57 +0300

    ampcor.ux: {layout} pull all its {views} from the {index}

--- ux/ampcor/client/views/layout/index.js
+++ ux/ampcor/client/views/layout/index.js
@@ -3,58 +3,60 @@
 // michael a.g. aïvázis <michael.aivazis@para-sim.com>
 // parasim
 // (c) 1998-2020 all rights reserved
-//
 
 
 // externals
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { useLazyLoadQuery } from 'react-relay/hooks'
 
 
 // locals
 // styles
 import styles from './styles'
+import { FlowContext, useFlowConfigQuery } from '~/context'
 // common to all pages
 import Header from './header'
 import Footer from './footer'
-// landing pages
-import Flow from '../flow'
-import EXP from '../exp'
-import SLC from '../slc'
-import Gamma from '../gamma'
-import Offsets from '../offsets'
-import Plan from '../plan'
-import Loading from '../loading'
-import Stop from '../stop'
+
+// views
+import { Flow, EXP, SLC, Gamma, Offsets, Plan, Loading, Stop } from '~/views'
 
 
 // the layout
-const Layout = ({version, flow}) => (
-    <Router>
-        <div style={styles.layout}>
-            <Header/>
-            <Switch>
-                {/* the top level views */}
-                <Route path="/flow">
-                    <Flow flow={flow}/>
-                </Route>
-                <Route path="/exp" component={EXP}/>
-                <Route path="/slc" component={SLC}/>
-                <Route path="/gamma" component={Gamma}/>
-                <Route path="/offsets" component={Offsets}/>
-                <Route path="/plan" component={Plan}/>
-                <Route path="/loading" component={Loading}/>
-                {/* the closing page */}
-                <Route path="/stop" component={Stop}/>
-                {/* default landing spot */}
-                <Route path="/" >
-                    <Flow flow={flow}/>
-                </Route>
-            </Switch>
-            <Footer version={version}/>
-        </div>
-    </Router>
-)
+const Layout = () => {
+    //
+    const data = useFlowConfigQuery()
+
+    // render
+    return (
+        <FlowContext.Provider value={data.flow}>
+            <Router>
+                <div style={styles.layout}>
+                    <Header/>
+                    <Switch>
+                        {/* the top level views */}
+                        <Route path="/flow" component={Flow}/>
+                        <Route path="/exp" component={EXP}/>
+                        <Route path="/slc" component={SLC}/>
+                        <Route path="/gamma" component={Gamma}/>
+                        <Route path="/offsets" component={Offsets}/>
+                        <Route path="/plan" component={Plan}/>
+
+                        {/* the closing page */}
+                        <Route path="/stop" component={Stop}/>
+                        {/* the page to render while waiting for data to arrive */}
+                        <Route path="/loading" component={Loading}/>
+
+                        {/* default landing spot */}
+                        <Route path="/" component={Flow}/>
+                    </Switch>
+                    <Footer />
+                </div>
+            </Router>
+        </FlowContext.Provider>
+    )
+}
 
 
 // publish
