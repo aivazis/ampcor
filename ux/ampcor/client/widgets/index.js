c89f2f4dabb8ec470eda35473073ca9a142e80c7
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-10-01 13:52:55 +0300

    ampcor.ux: {widgets} now have an {index} so clients are not expoosed to the file layout

---
+++ ux/ampcor/client/widgets/index.js
@@ -0,0 +1,13 @@
+// -*- web -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// parasim
+// (c) 1998-2020 all rights reserved
+
+
+export Flame from './flame'
+export Kill from './kill'
+export Server from './server'
+
+
+// end of file
