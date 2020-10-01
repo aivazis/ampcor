1f03cb4fa8ba72bfd35c8e60a004b2290b6c44e1
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-10-01 13:52:05 +0300

    ampcor.ux: the {server} widget runs its own query and doesn't depend on any data being threaded down to it

--- ux/ampcor/client/widgets/server/index.js
+++ ux/ampcor/client/widgets/server/index.js
@@ -7,41 +7,39 @@
 
 // externals
 import React from 'react'
-import { graphql, useFragment } from 'react-relay/hooks'
+import { graphql, useLazyLoadQuery } from 'react-relay/hooks'
 
 // locals
 import styles from './styles'
 
 
 // display the server state
-const server = ({version, style}) => {
+const server = ({style, ...props}) => {
     // the query fragment i care about
-    const data = useFragment(
-        graphql`fragment server_version on Version {
-            major
-            minor
-            micro
-            revision
-        }`,
-        version
+    const { version: {major, minor, micro, revision} } = useLazyLoadQuery(
+        graphql`query serverQuery {
+            version {
+                major
+                minor
+                micro
+                revision
+            }
+        }`
     )
 
     // merge the overall styles
     const base = {...style.box, ...styles.box, ...style.text, ...styles.text}
     // and the state colorization
-    const statusGood = {...style.status.good, ...styles.status.good}
+    const good = {...style.status.good, ...styles.status.good}
 
     // get the time
     const now = new Date()
     // use it to make a timestamp
     const title = `last checked on ${now.toString()}`
 
-    // unpack
-    const { major, minor, micro, revision } = data
-
     // build the componnent and return it
     return (
-        <div style={{...base, ...statusGood}} title={title}>
+        <div style={{...base, ...good}} title={title}>
             ampcor server {major}.{minor}.{micro} rev {revision}
         </div>
     )
