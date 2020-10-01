a0045010b5e4188f8973eb7c76bbd4a88a875e15
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-10-01 14:02:23 +0300

    ampcor.ux: all {views} are now published by the {index}

--- ux/ampcor/client/views/index.js
+++ ux/ampcor/client/views/index.js
@@ -3,10 +3,18 @@
 // michael a.g. aïvázis <michael.aivazis@para-sim.com>
 // parasim
 // (c) 1998-2020 all rights reserved
-//
 
 
 // publish
+export Flow from './flow'
+export EXP from './exp'
+export SLC from './slc'
+export Gamma from './gamma'
+export Offsets from './offsets'
+export Plan from './plan'
+export Loading from './loading'
+export Stop from './stop'
+
 export Layout from './layout'
 
 
