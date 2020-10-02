1cf6467f32a2c76b99f766b02ef0f6254afddabe
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-10-02 10:10:43 +0300

    ampcor.ux: the {flow} page went back to being boring

--- ux/ampcor/client/views/flow/index.js
+++ ux/ampcor/client/views/flow/index.js
@@ -7,7 +7,6 @@
 
 // externals
 import React from 'react'
-import { graphql, useFragment } from 'react-relay/hooks'
 // locals
 import styles from './styles'
 import { useFlowContext } from '~/context'
@@ -15,26 +14,12 @@ import { useFlowContext } from '~/context'
 
 // the ampcor workflow
 const flow = () => {
-    // get the flow configuration
-    const config = useFlowContext()
-
-    // unpack
-    // the flow
-    const { name: flowName, family: flowFamily } = config
-    // the reference slc
-    const { name: refName, family: refFamily, shape: refShape} = config.reference
-    // and the secondary slc
-    const { name: secName, family: secFamily, shape: secShape} = config.secondary
-
+ 
     // build the container and return it
     return (
         <section style={styles.flow}>
             <div style={styles.placeholder}>
-                <ul>
-                    <li>flow: {flowName}</li>
-                    <li>ref: {refName}</li>
-                    <li>sec: {secName}</li>
-                </ul>
+                the flow
             </div>
         </section>
     )
