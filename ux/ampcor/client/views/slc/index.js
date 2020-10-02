c02d96fbda559eba9f18a72df796a041ddb00ad7
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-10-02 10:27:28 +0300

    ampcor.ux: the {slc} component now retrieves the reference and secondary raster shapes from the {relay} context, which is in turn connected to the {pyre} configuration store

--- ux/ampcor/client/views/slc/index.js
+++ ux/ampcor/client/views/slc/index.js
@@ -9,34 +9,41 @@
 import React from 'react'
 // locals
 import styles from './styles'
+import { useFlowContext } from '~/context'
 
 
 // explore the input SLCs
 const slc = (props) => {
+    // get the flow configuration
+    const config = useFlowContext()
+    // unpack
+    // the reference slc
+    const { shape: refShape } = config.reference
+    // and the secondary slc
+    const { shape: secShape } = config.secondary
+
     // unputs that are hardwired for now
     // the zoom level
     const zoom = 0
-    // the raster shape
-    const rasterShape = [ 36864, 10344 ]
     // our tile shape
-    const tileShape = [ 512, 512 ]
+    const tileShape = [512, 512]
 
     // this is the shape of the slc, rounded down to the nearest tile multiple
     // N.B.: this clips the tiles that ride on the right and bottom margin of the data set;
     // they aren't whole tiles, so they either need a dynamically generated margin or more
     // complicated request logic
-    const gridShape = Array(2).fill().map((_, i) => Math.floor(rasterShape[i]/tileShape[i]))
+    const gridShape = [0, 1].map(i => Math.floor(refShape[i] / tileShape[i]))
     //  the plot shape rounded down to the nearest tile
-    const plotShape = Array(2).fill().map((_, i) => gridShape[i]*tileShape[i])
+    const plotShape = [0, 1].map(i => gridShape[i] * tileShape[i])
 
     // make a copy of the styling of the bitmap container
-    var plotStyle = {...styles.plot}
+    var plotStyle = { ...styles.plot }
     // add size information to the {plot} style sheet
     plotStyle.width = `${plotShape[1]}px`
     plotStyle.height = `${plotShape[0]}px`
 
     // make a copy of the styling of individual tiles
-    var tileStyle = {...styles.tile}
+    var tileStyle = { ...styles.tile }
     // and record the {tile} shape
     tileStyle.width = `${tileShape[1]}px`
     tileStyle.height = `${tileShape[0]}px`
@@ -63,14 +70,14 @@ const slc = (props) => {
     const cartesian = (first, ...rest) =>
         rest.reduce(
             (prefix, arg, foo) =>
-                prefix.flatMap( prefixEntry =>
-                    arg.map(idx => [prefixEntry, tileShape[foo+1]*idx].flat())
+                prefix.flatMap(prefixEntry =>
+                    arg.map(idx => [prefixEntry, tileShape[foo + 1] * idx].flat())
                 ),
             // convert {first} into an array of consecutive integers
-            first.map(idx => tileShape[0]*idx)
+            first.map(idx => tileShape[0] * idx)
         )
     // build an array of indices
-    const indices = Array(2).fill().map((_, i) => Array(gridShape[i]).fill().map((_, j) => j))
+    const indices = [0, 1].map(i => Array(gridShape[i]).fill().map((_, j) => j))
     // use this to build an array of tile origin in SLC coordinates
     const tileOrigins = cartesian(...indices)
 
@@ -91,8 +98,8 @@ const slc = (props) => {
                                   alt={tileTitle(origin)}
                                   style={tileStyle}
                              />
-                         )}
-                     )}
+                         )
+                     })}
                 </div>
             </div>
         </section>
