706ee2efbc277b7b184f70d294b87c513ea9f143
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-10-10 09:14:02 +0300

    ampcor.ux: the {slc} voew now deploys an {slc} panel to do the rendering; it remains responsible for the selection of which data product to display

--- ux/ampcor/client/views/slc/index.js
+++ ux/ampcor/client/views/slc/index.js
@@ -10,8 +10,7 @@ import React, { useState } from 'react'
 // locals
 import styles from './styles'
 import { useFlowContext } from '~/context'
-import SLC from '~/widgets/slc'
-import Zoom from '~/widgets/zoom'
+import { SLC } from '~/panels'
 
 
 // explore the input SLCs
@@ -24,107 +23,15 @@ const slc = (props) => {
     // select the slc to show
     const [slc, setSLC] = useState("ref")
     // extract its shape
-    const rasterShape = rasterShapes[slc]
-
-    // our tile shape; optimized so that each line is one page
-    const tileShape = [512, 512]
-
-    // the zoom level suggestions for each axis
-    const maxZoom  = Math.min(
-        ...rasterShape.map((sz, i) => Math.floor(Math.log2(sz/tileShape[i])))
-    )
-
-    // start out at maximum zoom
-    const [zoom, setZoom] = useState(maxZoom)
-    // convert the zoom level to a scaling factor
-    const scale = 1 << zoom;
-
-    // the zoom in callback; clip at zoom level 0
-    const zoomin = () => (zoom > 0 ? setZoom(zoom-1) : null)
-    // the zoom out callback; clip at {maxZoom}
-    const zoomout = () => (zoom < maxZoom ? setZoom(zoom+1) : null)
-
-    // pick a signal to show
-    const [signal, setSignal] = useState("amplitude")
-
-    // this is the shape of the slc, rounded down to the nearest tile multiple
-    // N.B.: this clips the tiles that ride on the right and bottom margin of the data set;
-    // they aren't whole tiles, so they either need a dynamically generated margin or more
-    // complicated request logic
-    const gridShape = [0, 1].map(i => Math.floor(rasterShape[i] / tileShape[i] / scale))
-    //  the plot shape rounded down to the nearest tile
-    const plotShape = [0, 1].map(i => gridShape[i] * tileShape[i])
-
-    // make a copy of the styling of the bitmap container
-    var plotStyle = { ...styles.plot }
-    // add size information to the {plot} style sheet
-    plotStyle.width = `${plotShape[1]}px`
-    plotStyle.height = `${plotShape[0]}px`
-
-    // make a copy of the styling of individual tiles
-    var tileStyle = { ...styles.tile }
-    // and record the {tile} shape
-    tileStyle.width = `${tileShape[1]}px`
-    tileStyle.height = `${tileShape[0]}px`
-
-    // tile specification factory
-    const assembleTileSpec = (first, ...rest) =>
-        rest.reduce(
-            (accum, val) => `${accum}x${val}`,
-            `${first}`
-        )
-
-    // assemble the tile shape spec
-    const tileShapeSpec = assembleTileSpec(...tileShape)
+    const shape = rasterShapes[slc]
 
     // build the tile uri stem
-    const baseURI = `/slc/${slc}/${signal}/${zoom}/${tileShapeSpec}/`
-
-    // given the origin of a tile, build its uri
-    const tileURI = (origin) => baseURI + assembleTileSpec(...origin)
-    // and its title; disabled, for now
-    // const tileTitle = (origin) => `(${origin})+(${tileShapeSpec})`
-
-    // the cartesian product calculator
-    const cartesian = (first, ...rest) =>
-        rest.reduce(
-            (prefix, arg, foo) =>
-                prefix.flatMap(prefixEntry =>
-                    arg.map(idx => [prefixEntry, tileShape[foo + 1] * idx].flat())
-                ),
-            // convert {first} into an array of consecutive integers
-            first.map(idx => tileShape[0] * idx)
-        )
-    // build an array of indices
-    const indices = [0, 1].map(i => Array(gridShape[i]).fill().map((_, j) => j))
-    // use this to build an array of tile origin in SLC coordinates
-    const tileOrigins = cartesian(...indices)
+    const uri = `/slc/${slc}`
 
     // build the container, fill it, and return it
     return (
         <section style={styles.slc}>
-            <div style={styles.viewport}>
-                {/* the SLC signal toolbox */}
-                <SLC select={setSignal} style={styles.slcToolbox} />
-                {/* the zoom toolbox */}
-                <Zoom zoomin={zoomin} zoomout={zoomout} style={styles.zoomToolbox} />
-                {/* the SLC signal rendering area */}
-                <div style={plotStyle} >
-                    {tileOrigins.map(origin => {
-                         // assemble the tile name
-                         const key = tileURI(origin)
-                         // the tile uri
-                         const uri = tileURI(origin)
-                         // render
-                         return (
-                             <img key={key}
-                                  loading="lazy" className="lazyload" data-src={uri}
-                                  style={tileStyle}
-                             />
-                         )
-                     })}
-                </div>
-            </div>
+            <SLC uri={uri} shape={shape} style={styles?.slcPanel} />
         </section>
     )
 }
--- ux/ampcor/client/views/slc/styles.js
+++ ux/ampcor/client/views/slc/styles.js
@@ -11,126 +11,12 @@ import { wheel, theme } from '~/palette'
 import base from '~/views/styles'
 
 
-// tool styling; see '~/widgets/tool/*.js' for details
-const tool = {
-    // the base state
-    base: {
-        box: {
-            flex: "none",
-            width: "2em",
-            height: "2em",
-            color: theme.banner.name,
-            // backgroundColor: theme.page.background,
-        },
-
-        group: {
-            transform: "scale(0.4)",
-        },
-
-        frame: {
-            fill: wheel.gray.obsidian,
-            fillOpacity: 0.4,
-            stroke: wheel.gray.steel,
-            strokeOpacity: 0.75,
-            strokeWidth: "5px",
-        },
-
-        icon: {
-            stroke: theme.banner.name,
-            strokeOpacity: 0.75,
-            strokeWidth: "5px",
-        },
-
-    },
-    inactive: {
-        frame: {
-            fillOpacity: .2,
-        },
-        icon: {
-            strokeOpacity: .2,
-        },
-    },
-    selected: {
-        frame: {
-            fillOpacity: 1.0,
-            stroke: theme.banner.name,
-        },
-    }
-}
-
-
 // publish
 export default {
     // the container
     slc: {
         // inherit
         ...base.panel,
-    },
-
-    viewport: {
-        // inherit
-        ...base.viewport,
-    },
-
-    plot: {
-        flex: "none",
-        display: "flex",
-        flexDirection: "row",
-        flexWrap: "wrap",
-        overflow: "hidden",
-    },
-
-    tile: {
-        flex: "none",
-        // the sizes here are defaults; they get overriden when more/better is known
-        width: "256px",
-        height: "256px",
-    },
-
-    // slc tool box styling
-    slcToolbox: {
-        toolbox: {
-            // positioning
-            position: "absolute",
-            right: "1.0em",
-            top: "0.75em",
-            // size
-            width: "2.0em",
-            height: "6.3em", // 3 tools at 2em each, plus some space in between
-            // styling
-            opacity: "0.75",
-            // backgroundColor: theme.page.background,
-            // border: `1px solid ${wheel.gray.basalt}`,
-        },
-
-        // tool styling; see '~/widgets/tool/*.js' for details
-        tool: {
-            // start with the default established up top
-            ...tool
-        },
-    },
-
-    // slc tool box styling
-    zoomToolbox: {
-        toolbox: {
-            // positioning
-            position: "absolute",
-            right: "1.0em",
-            bottom: "0.75em",
-            // size
-            width: "2.0em",
-            height: "4.0em", // 2 tools at 2em each, no space in between
-            // styling
-            opacity: "0.75",
-            // backgroundColor: theme.page.background,
-            // border: `1px solid ${wheel.gray.basalt}`,
-        },
-
-        // tool styling; see '~/widgets/tool/*.js' for details
-        tool: {
-            // start with the default established up top
-            ...tool
-        },
     },
 
     placeholder: {
