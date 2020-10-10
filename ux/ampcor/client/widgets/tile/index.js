b0d29e8e43223ce2794de38f0475513590d74b22
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-10-10 09:00:27 +0300

    ampcor.ux: added a {tile} widget that encpasulates the rendering of individual bitmaps

---
+++ ux/ampcor/client/widgets/tile/index.js
@@ -0,0 +1,31 @@
+// -*- web -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// parasim
+// (c) 1998-2020 all rights reserved
+
+
+// externals
+import React, { useState } from 'react'
+// locals
+import styles from './style'
+
+
+// currently, a tile is an {img} that's lazy loaded from a {uri}
+const widget = ({uri, style}) => {
+    // mix the styling
+    const tileStyle = { ...styles, ...style }
+    // render
+    return (
+        <img loading="lazy" className="lazyload" data-src={uri}
+             style={tileStyle}
+        />
+    )
+}
+
+
+// publish
+export default widget
+
+
+// end of file
---
+++ ux/ampcor/client/widgets/tile/style.js
@@ -0,0 +1,23 @@
+// -*- web -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// parasim
+// (c) 1998-2020 all rights reserved
+
+
+// get colors
+import { wheel, theme } from '~/palette'
+
+
+// publish
+export default {
+    // i'm not resizable; my parent uses flex to position me
+    flex: "none",
+    // size defaults; the parent should override
+    // must be in pixels
+    width: "256px",
+    height: "256px",
+}
+
+
+// end of file
