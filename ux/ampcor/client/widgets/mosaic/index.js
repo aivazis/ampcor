5667204fa2112166937b72428f6fa8d74d8717fd
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-10-10 15:15:50 +0300

    ampcor.ux: the {mosaic} wodget can now center its view on the position of the mouse when the user double clicks

--- ux/ampcor/client/widgets/mosaic/index.js
+++ ux/ampcor/client/widgets/mosaic/index.js
@@ -7,14 +7,14 @@
 
 // externals
 import path from 'path'
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 // locals
 import styles from './style'
 import { Tile } from 'widgets'
 
 
 // currently, a tile is an {img} that's lazy loaded from a {uri}
-const widget = ({uri, origins, style}) => {
+const widget = ({uri, origins, zoomin, style}) => {
     // content styling
     // mix the viewport style
     const viewportStyle = {
@@ -42,9 +42,43 @@ const widget = ({uri, origins, style}) => {
     // given the origin of a tile, build its uri
     const tileURI = (origin) => path.join(uri, foldRanks(...origin))
 
+    // make a reference to attach to my container
+    const viewportRef = useRef(null)
+    // on double click, center to the mouse coordinates
+    const center = ({clientX, clientY}) => {
+        // get the viewport
+        const viewport = viewportRef?.current
+        // if it has been rendered
+        if (viewport) {
+            // get the viewport boundinbox
+            const box = viewport.getBoundingClientRect()
+
+            // compute the location of the click relative to the viewport
+            const x = clientX - box.left
+            const y = clientY - box.top
+            // get the size of the viewport
+            const width = viewport.clientWidth
+            const height = viewport.clientHeight
+            // get the current scroll position
+            const left = viewport.scrollLeft
+            const top = viewport.scrollTop
+            // if i have a valid zoom callback
+            if (zoomin) {
+                // invoke it
+                // zoomin()
+            }
+            // and scroll to the new location
+            viewport.scroll({
+                top: top + y - height/2,
+                left: left + x - width/2,
+                behavior: "smooth",
+            })
+        }
+    }
+
     // render
     return (
-        <div style={viewportStyle} >
+        <div ref={viewportRef} style={viewportStyle} onDoubleClick={center} >
             <div style={mosaicStyle} >
                 {origins.map(origin => {
                      // form the tile uri
