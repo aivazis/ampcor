acc546c5322d59cd6fe72da5fd7b90f868632133
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-10-12 13:42:57 +0300

    ampcor.ux: added {namedtools}, a widget that displays a set of strings as a toolbox whose tool icons are the elements of the set

---
+++ ux/ampcor/client/widgets/namedtools/index.js
@@ -0,0 +1,68 @@
+// -*- web -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// parasim
+// (c) 1998-2020 all rights reserved
+
+
+// externals
+import React, { useState } from 'react'
+// locals
+import styles from './style'
+import { Tool } from '~/widgets'
+
+
+// a selector of a prdouct from a "stack"
+const stack = ({select, style, choices}) => {
+    // state management
+    // the default selection
+    const [selection, setSelection] = useState(choices[0])
+    // all entries are active by default
+    const stateTable = choices.reduce(
+        (partial, choice) => {
+            // mark the current choice as active
+            partial[choice] = "active"
+            // and return the updated object
+            return partial
+        },
+        // starting with the empty object
+        {}
+    )
+    // and one is selected
+    stateTable[selection] = "selected"
+
+    // when a tool is clicked, two things have to happen:
+    const pick = (selection) => {
+        // the local selection must be updated
+        setSelection(selection)
+        // and we have to notify the client
+        select(selection)
+    }
+
+    // mix the toolbox style
+    const toolboxStyle = { ...styles.toolbox, ...style }
+
+    // render
+    return (
+        <div style={toolboxStyle}>
+            {choices.map(choice => {
+                 // look up the state
+                 const state = stateTable[choice]
+                 // make an {onClick} callback
+                 const choose = () => pick(choice)
+                 // render
+                 return (
+                     <Tool key={choice} click={choose} state={state} style={styles.tool} >
+                         <text x="12" y="65">{choice}</text>
+                     </Tool>
+            )})}
+        </div>
+    )
+}
+
+
+// publish
+export default stack
+
+
+// end of file
---
+++ ux/ampcor/client/widgets/namedtools/style.js
@@ -0,0 +1,82 @@
+// -*- web -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// parasim
+// (c) 1998-2020 all rights reserved
+
+
+// get colors
+import { wheel, theme } from '~/palette'
+
+
+// tool styling; see '~/widgets/tool/*.js' for details
+const tool = {
+    // the base state
+    base: {
+        box: {
+            flex: "none",
+            width: "2em",
+            height: "2em",
+            color: theme.banner.name,
+            // backgroundColor: theme.page.background,
+        },
+
+        group: {
+            transform: "scale(0.4)",
+        },
+
+        frame: {
+            fill: wheel.gray.obsidian,
+            fillOpacity: 0.4,
+            stroke: wheel.gray.steel,
+            strokeOpacity: 0.75,
+            strokeWidth: "5px",
+        },
+
+        icon: {
+            fontFamily: "inconsolata",
+            fontSize: "50px",
+            fill: theme.banner.name,
+            stroke: theme.banner.name,
+            strokeOpacity: 0.75,
+            strokeWidth: "1px",
+        },
+
+    },
+    inactive: {
+        frame: {
+            fillOpacity: .2,
+        },
+        icon: {
+            strokeOpacity: .2,
+        },
+    },
+    selected: {
+        frame: {
+            fillOpacity: 1.0,
+            stroke: theme.banner.name,
+        },
+    }
+}
+
+
+// publish
+export default {
+    toolbox: {
+        // for my children
+        display: "flex",
+        // vertical box, by default
+        flexDirection: "row",
+        // spread them out
+        justifyContent: "space-between",
+    },
+
+    // tool styling; see '~/widgets/tool/*.js' for details
+    tool: {
+        // start with the default established up top
+        ...tool
+    },
+}
+
+
+// end of file
