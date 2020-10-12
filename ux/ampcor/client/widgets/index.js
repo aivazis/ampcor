ae03d574416463b640cefb7902d460a9f1e89497
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-10-12 13:43:20 +0300

    ampcor.ux: published the new {namedtools} widget

--- ux/ampcor/client/widgets/index.js
+++ ux/ampcor/client/widgets/index.js
@@ -8,9 +8,11 @@
 export Flame from './flame'
 export Kill from './kill'
 export Mosaic from './mosaic'
+export NamedTools from './namedtools'
 export SLC from './slc'
 export Server from './server'
 export Tile from './tile'
+export Tool from './tool'
 export Zoom from './zoom'
 
 
