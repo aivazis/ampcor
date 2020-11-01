ca64c36bb1b24de9794f2a5f9b1109fd8fd5ade3
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-11-01 12:28:40 -0800

    ampcor::cuda: stubbed out {sequential_t} to prep the implementation of the new interface

--- lib/ampcor_cuda/correlators/Sequential.h
+++ lib/ampcor_cuda/correlators/Sequential.h
@@ -12,157 +12,63 @@
 
 
 // resource management and orchestration of the execution of the correlation plan
-template <typename raster_t>
+template <class slcT, class offsetsT>
 class ampcor::cuda::correlators::Sequential {
     // types
 public:
-    // my client raster type
-    using raster_type = raster_t;
-    // views over it
-    using view_type = typename raster_type::view_type;
-    using constview_type = typename raster_type::constview_type;
-    // the underlying pixel complex type
-    using cell_type = typename raster_type::cell_type;
-    // the support of the pixel complex type
-    using value_type = typename cell_type::value_type;
-    // for describing slices of rasters
-    using slice_type = typename raster_type::slice_type;
-    // for describing the shapes of tiles
-    using shape_type = typename raster_type::shape_type;
-    // for describing the layouts of tiles
-    using layout_type = typename raster_type::layout_type;
-    // for index arithmetic
-    using index_type = typename raster_type::index_type;
-    // for sizing things
-    using size_type = typename raster_type::size_type;
 
-    // adapter for tiles within my arena
-    using tile_type = pyre::grid::grid_t<cell_type,
-                                         layout_type,
-                                         pyre::memory::view_t<cell_type>>;
+    // metamethods
+public:
+    // destructor
+    inline ~Sequential();
+    // constructor
+    inline
+    Sequential(int rank,
+               slc_const_reference ref, slc_const_reference sec,
+               slc_shape_const_reference chip, slc_shape_const_reference window,
+               int refineFactor, int refineMargin, int zoomFactor);
+
 
     // interface
 public:
-    // add a reference tile to the pile
-    inline void addReferenceTile(size_type pid, const constview_type & ref);
-    // add a secondary search window to the pile
-    inline void addSecondaryTile(size_type pid, const constview_type & sec);
 
-    // compute adjustments to the offset map
-    inline auto adjust() -> const value_type *;
-
-    // accessors
-    inline auto pairs() const -> size_type;
-    inline auto arena() const -> const cell_type *;
-
-    // debugging support
-    inline void dump() const;
-
-    // meta-methods
-public:
-    inline ~Sequential();
-    inline Sequential(size_type pairs,
-                      const layout_type & refLayout, const layout_type & secLayout,
-                      size_type refineFactor=2, size_type refineMargin=8,
-                      size_type zoomFactor=4);
-
-    // implementation details: methods
-public:
-    // push the tiles in the plan to device
-    inline auto _push() const -> cell_type *;
-    // compute the magnitude of the complex signal pixel-by-pixel
-    inline auto _detect(const cell_type * cArena,
-                        size_type refDim, size_type secDim) const -> value_type *;
-    // subtract the mean from reference tiles and compute the square root of their variance
-    inline auto _refStats(value_type * rArena,
-                          size_type refDim, size_type secDim) const -> value_type *;
-    // compute the sum area tables for the secondary tiles
-    inline auto _sat(const value_type * rArena,
-                     size_type refDim, size_type secDim) const -> value_type *;
-    // compute the mean of all possible placements of a tile the same size as the reference
-    // tile within the secondary
-    inline auto _secStats(const value_type * sat,
-                          size_type refDim, size_type secDim, size_type corDim
-                          ) const -> value_type *;
-    // correlate
-    inline auto _correlate(const value_type * rArena,
-                           const value_type * refStats, const value_type * secStats,
-                           size_type refDim, size_type secDim, size_type corDim
-                           ) const -> value_type *;
-    // find the locations of the maxima of the correlation matrix
-    inline auto _maxcor(const value_type * gamma, size_type corDim) const -> int *;
-    // adjust the locations of the maxima so that the refined tile sources fit with the secondary
-    inline void _nudge(int * locations, size_type refDim, size_type secDim) const;
-    // allocate memory for a new arena big enough to hold the refined tiles
-    inline auto _refinedArena() const -> cell_type *;
-    // refine the reference tiles
-    inline void _refRefine(cell_type * coarseArena, cell_type * refinedArena) const;
-    // migrate the expanded unrefined secondary tiles into the {refinedArena}
-    inline void _secMigrate(cell_type * coarseArena, int * locations,
-                            cell_type * refinedArena) const;
-    // refine the secondary tiles
-    inline void _secRefine(cell_type * refinedArena) const;
-    // deramp
-    inline void _deramp(cell_type * arena) const;
-    // zoom the correlation matrix
-    inline auto _zoomcor(value_type * gamma) const -> value_type *;
-    // assemble the offsets
-    inline auto _offsetField(const int * maxcor, const int * zoomed) -> const value_type *;
-
-    // unfinished correlation matrix zoom that uses R2C and C2R
-    inline auto _zoomcor_r2r(value_type * gamma) const -> value_type *;
-
-
-    // implementation details: data
+    // implementation
 private:
-    // my capacity, in {ref/sec} pairs
-    const size_type _pairs;
-    const size_type _refineFactor;
-    const size_type _refineMargin;
-    const size_type _zoomFactor;
 
-    // the shape of the reference tiles
-    const layout_type _refLayout;
-    // the shape of the search windows in the secondary image
-    const layout_type _secLayout;
-    // the shape of the correlation matrix
-    const layout_type _corLayout;
-    // the shape of the reference tiles after refinement
-    const layout_type _refRefinedLayout;
-    // the shape of the secondary tiles after refinement
-    const layout_type _secRefinedLayout;
-    // the shape of the correlation matrix after refinement
-    const layout_type _corRefinedLayout;
-    // the shape of the correlation matrix after zooming
-    const layout_type _corZoomedLayout;
+    // data
+private:
+    // my rank
+    int _rank;
+    // the input rasters
+    slc_const_reference _ref;
+    slc_const_reference _sec;
+    // the output raster
+    offsets_reference _map;
 
-    // the number of cells in a reference tile
-    const size_type _refCells;
-    // the number of cells in a secondary search window
-    const size_type _secCells;
-    // the number of cell in a correlation matrix
-    const size_type _corCells;
-    // the number of cells in a refined reference tile
-    const size_type _refRefinedCells;
-    // the number of cells in a refined secondary tile
-    const size_type _secRefinedCells;
+    // the shape of the tiles
+    const slc_shape_type _refShape;
+    const slc_shape_type _secShape;
 
-    // the number of bytes in a reference tile
-    const size_type _refFootprint;
-    // the number of bytes in a secondary search window
-    const size_type _secFootprint;
-    // the number of bytes in a correlation matrix
-    const size_type _corFootprint;
-    // the number of bytes in a refined reference tile
-    const size_type _refRefinedFootprint;
-    // the number of bytes in a refined secondary tile
-    const size_type _secRefinedFootprint;
+    // the correlation surface refinement parameters
+    const int _refineFactor;
+    const int _refineMargin;
+    const int _zoomFactor;
 
-    // host storage for the tile pairs
-    cell_type * const _arena;
-    // host storage for the offset field
-    value_type * const _offsets;
+    // disabled methods
+private:
+    // constructors
+    Sequential(const Sequential &) = delete;
+    Sequential(Sequential &&) = delete;
+    Sequential & operator=(const Sequential &) = delete;
+    Sequential & operator=(Sequential &&) = delete;
 };
+
+
+// the inline definitions
+// sequential
+#define ampcor_cuda_correlators_Sequential_icc
+#include "Sequential.icc"
+#undef ampcor_cuda_correlators_Sequential_icc
 
 
 // code guard
