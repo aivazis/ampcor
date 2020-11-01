c50579ad242805b9072b35ae1cc80757eff726d4
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-11-01 12:27:54 -0800

    ampcor::cuda: updated the namespace declarations of the {cuda} kernels

--- lib/ampcor_cuda/correlators/kernels.h
+++ lib/ampcor_cuda/correlators/kernels.h
@@ -10,80 +10,78 @@
 #if !defined(ampcor_libampcor_cuda_correlators_kernels_h)
 #define ampcor_libampcor_cuda_correlators_kernels_h
 
-// cuda
-#include <cuComplex.h>
-// STL
+
+// externals
 #include <complex>
+#include <cuComplex.h>
+
 
 // forward declarations
-namespace ampcor {
-    namespace cuda {
-        namespace kernels {
-            // compute amplitudes of the tile pixels
-            void detect(const std::complex<float> * cArena, std::size_t cells, float * rArena);
+namespace ampcor::cuda::kernels {
+    // compute amplitudes of the tile pixels
+    void detect(const std::complex<float> * cArena, std::size_t cells, float * rArena);
 
-            // subtract the tile mean from each reference pixel
-            void refStats(float * rArena,
-                          std::size_t pairs, std::size_t refDim, std::size_t cellsPerTilePair,
-                          float * stats);
+    // subtract the tile mean from each reference pixel
+    void refStats(float * rArena,
+                  std::size_t pairs, std::size_t refDim, std::size_t cellsPerTilePair,
+                  float * stats);
 
-            // build the sum area tables for the secondary tiles
-            void sat(const float * rArena,
+    // build the sum area tables for the secondary tiles
+    void sat(const float * rArena,
+             std::size_t pairs,
+             std::size_t refCells, std::size_t secCells, std::size_t secDim,
+             float * sat);
+
+    // compute the average amplitude for all possible placements of a reference shape
+    // within the search windows
+    void secStats(const float * sat,
+                  std::size_t pairs,
+                  std::size_t refDim, std::size_t secDim, std::size_t corDim,
+                  float * stats);
+
+    // compute the correlation matrix
+    void correlate(const float * rArena, const float * refStats, const float * secStats,
+                   std::size_t pairs,
+                   std::size_t refCells, std::size_t secCells, std::size_t corCells,
+                   std::size_t refDim, std::size_t secDim, std::size_t corDim,
+                   float * dCorrelation);
+
+    // compute the locations of the maximum value of the correlation map
+    void maxcor(const float * cor,
+                std::size_t pairs, std::size_t corCells, std::size_t corDim,
+                int * loc);
+
+    // nudge the (row, col) pairs so that they describe sub-tiles within a secondary tile
+    void nudge(std::size_t pairs,
+               std::size_t refDim,  std::size_t secDim, std::size_t margin,
+               int * locations);
+
+    // migrate the expanded maxcor tiles to the refinement arena
+    void migrate(const std::complex<float>  * arena,
+                 std::size_t pairs,
+                 std::size_t refDim, std::size_t secDim, std::size_t expDim,
+                 std::size_t refRefinedDim, std::size_t secRefinedDim,
+                 const int * locations,
+                 std::complex<float> * refinedArena);
+
+    // upcast the correlation matrix into complex numbers and embed in the zoomed
+    // hyper-matrix
+    auto r2c(const float * gamma,
+             std::size_t pairs, std::size_t corDim, std::size_t zmdDim
+             ) -> cuComplex *;
+
+    // convert the zoomed correlation matrix to floats
+    auto c2r(const cuComplex * scratch,
+             std::size_t pairs, std::size_t zmdDim
+             ) -> float *;
+
+    // assemble the offset field
+    void offsetField(const int * maxcor, const int * zoomed,
                      std::size_t pairs,
-                     std::size_t refCells, std::size_t secCells, std::size_t secDim,
-                     float * sat);
-
-            // compute the average amplitude for all possible placements of a reference shape
-            // within the search windows
-            void secStats(const float * sat,
-                          std::size_t pairs,
-                          std::size_t refDim, std::size_t secDim, std::size_t corDim,
-                          float * stats);
-
-            // compute the correlation matrix
-            void correlate(const float * rArena, const float * refStats, const float * secStats,
-                           std::size_t pairs,
-                           std::size_t refCells, std::size_t secCells, std::size_t corCells,
-                           std::size_t refDim, std::size_t secDim, std::size_t corDim,
-                           float * dCorrelation);
-
-            // compute the locations of the maximum value of the correlation map
-            void maxcor(const float * cor,
-                        std::size_t pairs, std::size_t corCells, std::size_t corDim,
-                        int * loc);
-
-            // nudge the (row, col) pairs so that they describe sub-tiles within a secondary tile
-            void nudge(std::size_t pairs,
-                       std::size_t refDim,  std::size_t secDim, std::size_t margin,
-                       int * locations);
-
-            // migrate the expanded maxcor tiles to the refinement arena
-            void migrate(const std::complex<float>  * arena,
-                         std::size_t pairs,
-                         std::size_t refDim, std::size_t secDim, std::size_t expDim,
-                         std::size_t refRefinedDim, std::size_t secRefinedDim,
-                         const int * locations,
-                         std::complex<float> * refinedArena);
-
-            // upcast the correlation matrix into complex numbers and embed in the zoomed
-            // hyper-matrix
-            auto r2c(const float * gamma,
-                     std::size_t pairs, std::size_t corDim, std::size_t zmdDim
-                     ) -> cuComplex *;
-
-            // convert the zoomed correlation matrix to floats
-            auto c2r(const cuComplex * scratch,
-                     std::size_t pairs, std::size_t zmdDim
-                     ) -> float *;
-
-            // assemble the offset field
-            void offsetField(const int * maxcor, const int * zoomed,
-                             std::size_t pairs,
-                             std::size_t margin, std::size_t refineMargin, std::size_t zoom,
-                             float * field);
-        }
-    }
+                     std::size_t margin, std::size_t refineMargin, std::size_t zoom,
+                     float * field);
 }
+
 
 // code guard
 #endif
