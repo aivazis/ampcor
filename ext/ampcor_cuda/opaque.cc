0279d9836c0fd39d339e78bf00a46db8fc99520c
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-11-01 15:47:16 -0800

    ampcor::cuda: added stubs for the package {exceptions} and {opaque} data types

---
+++ ext/ampcor_cuda/opaque.cc
@@ -0,0 +1,22 @@
+// -*- c++ -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2020 all rights reserved
+
+
+// externals
+#include "external.h"
+// namespace setup
+#include "forward.h"
+
+
+// opaque classes do not get auto-converted to and from python
+void
+ampcor::cuda::py::
+opaque(py::module & m) {
+    // all done
+    return;
+}
+
+
+// end of file
