90680ffe2b33401dba23a724b3a0d438b766b2a6
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-11-01 15:47:58 -0800

    ampcor::cuda: added the refactored package header files

---
+++ ext/ampcor_cuda/forward.h
@@ -0,0 +1,25 @@
+// -*- c++ -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2020 all rights reserved
+
+// code guard
+#if !defined(ampcor_cuda_py_forward_h)
+#define ampcor_cuda_py_forward_h
+
+
+// the {ampcor} namespace
+namespace ampcor::cuda::py {
+    // bindings of opaque types
+    void opaque(py::module &);
+    // exceptions
+    void exceptions(py::module &);
+
+    // access to the correlators
+    void sequential(py::module &);
+}
+
+
+#endif
+
+// end of file
