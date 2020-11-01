67c5ed875069665d872e811dba1668025a58a16b
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-11-01 15:49:35 -0800

    ampcor::cuda: added the {python} module initializer

--- ext/ampcor_cuda/ampcor_cuda.cc
+++ ext/ampcor_cuda/ampcor_cuda.cc
@@ -1,86 +1,28 @@
-// -*- C++ -*-
+// -*- c++ -*-
 //
 // michael a.g. aïvázis <michael.aivazis@para-sim.com>
-// parasim
 // (c) 1998-2020 all rights reserved
-//
-
-#include <portinfo>
-#include <Python.h>
-
-// the module method declarations
-#include "exceptions.h"
-#include "metadata.h"
-#include "sequential.h"
 
 
-// put everything in my private namespace
-namespace ampcor {
-    namespace extension {
-        namespace cuda {
-
-            // the module method table
-            PyMethodDef module_methods[] = {
-                 // the copyright method
-                 { copyright__name__, copyright, METH_VARARGS, copyright__doc__ },
-                 // the version
-                 { version__name__, version, METH_VARARGS, version__doc__ },
-
-                 // image registration
-                 { sequential::alloc__name__,
-                   sequential::alloc, METH_VARARGS, sequential::alloc__doc__},
-
-                 { sequential::addReference__name__,
-                   sequential::addReference, METH_VARARGS, sequential::addReference__doc__},
-
-                 { sequential::addSecondary__name__,
-                   sequential::addSecondary, METH_VARARGS, sequential::addSecondary__doc__},
-
-                 { sequential::adjust__name__,
-                   sequential::adjust, METH_VARARGS, sequential::adjust__doc__},
-
-                 // sentinel
-                 { 0, 0, 0, 0 }
-            };
-
-            // the module documentation string
-            const char * const __doc__ = "ampcor: a tool for benchmarking ampcor implementations";
-
-            // the module definition structure
-            PyModuleDef module_definition = {
-                // header
-                PyModuleDef_HEAD_INIT,
-                // the name of the module
-                "ampcor_cuda",
-                // the module documentation string
-                __doc__,
-                // size of the per-interpreter state of the module; -1 if this state is global
-                -1,
-                // the methods defined in this module
-                module_methods
-            };
-
-        } // of namespace cuda
-    } // of namespace extension
-} // of namespace ampcor
+// external dependencies
+#include "external.h"
+// namespace setup
+#include "forward.h"
 
 
-// initialization function for the module
-// *must* be called PyInit_ampcor_cuda
-PyMODINIT_FUNC
-PyInit_ampcor_cuda()
-{
-    // create the module
-    PyObject * module = PyModule_Create(&ampcor::extension::cuda::module_definition);
-    // check whether module creation succeeded and raise an exception if not
-    if (!module) {
-        return module;
-    }
+// the module entry point
+PYBIND11_MODULE(ampcor_cuda, m) {
+    // the doc string
+    m.doc() = "the libampcor_cuda bindings";
 
-    // otherwise, we have an initialized module; register the package exceptions
-    ampcor::extension::cuda::registerExceptionHierarchy(module);
-    // and return the newly created module
-    return module;
+    // bind the opaque types
+    ampcor::cuda::py::opaque(m);
+    // register the exception types
+    ampcor::cuda::py::exceptions(m);
+
+    // correlators
+    ampcor::cuda::py::sequential(m);
 }
+
 
 // end of file
