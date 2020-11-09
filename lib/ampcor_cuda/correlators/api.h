6386ee52f9cc09f1da6b55b6b688ce4819718dee
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-11-09 09:12:00 -0800

    ampcor::cuda::correlators: added type aliases for grids over device memory; the goal for now is to pass pointers and shapes around in a convenient way

--- lib/ampcor_cuda/correlators/api.h
+++ lib/ampcor_cuda/correlators/api.h
@@ -11,12 +11,25 @@
 
 // publicly visible types
 namespace ampcor::cuda::correlators {
+    // make a selector
+    template <typename T, bool isReadOnly>
+    using cudaheap_t = CUDAHeap<T, isReadOnly>;
+
     // device memory
     template <typename T>
-    using cudaheap_t = CUDAHeap<T, false>;
+    using devmem_t = CUDAHeap<T, false>;
     // read-only version
     template <typename T>
-    using constcudaheap_t = CUDAHeap<T, true>;
+    using constdevmem_t = CUDAHeap<T, true>;
+
+    // a grid on the device
+    template <typename specT, bool isReadOnly = true>
+    using devgrid_t = pyre::grid::grid_t<typename specT::layout_type,
+                                         cudaheap_t<typename specT::pixel_type, isReadOnly>>;
+
+    // an arena on the device
+    template <typename pixelT>
+    using devarena_raster_t = devgrid_t<dom::arena_t<pixelT>, false>;
 
     // the worker
     template <class slcT = dom::slc_const_raster_t, class offsetsT = dom::offsets_raster_t>
