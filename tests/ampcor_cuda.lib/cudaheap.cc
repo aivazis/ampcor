6005b3a458892c212346f25910b752704c46e86b
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-11-15 19:42:56 -0800

    ampcor::cuda::correlators: the {cudaheap} test case now writes to the device allocation, and checks that the value is retrievable

--- tests/ampcor_cuda.lib/cudaheap.cc
+++ tests/ampcor_cuda.lib/cudaheap.cc
@@ -14,7 +14,7 @@
 
 // type aliases
 // device memory
-using heap_t = ampcor::cuda::correlators::cudaheap_t<float>;
+using heap_t = ampcor::cuda::correlators::cudaheap_t<float, false>;
 
 
 // an allocation function
@@ -25,7 +25,7 @@ auto alloc(size_t cells)
 }
 
 
-// use a device allocation
+// read from a device allocation
 auto read(heap_t block)
 {
     // read from the start
@@ -33,26 +33,51 @@ auto read(heap_t block)
 }
 
 
+// write to a device allocation
+auto write(heap_t block, float value)
+{
+    // write to the start
+    block[0] = value;
+    // all done
+    return;
+}
+
+
 // make a scope for the allocation
 void test()
 {
     // make a channel
-    pyre::journal::info_t channel("ampcor.cuda");
+    pyre::journal::debug_t channel("ampcor.cuda");
 
     // sign in
     channel
         << "test: sanity check for cuda device memory allocation"
         << pyre::journal::endl(__HERE__);
 
+    // make a value
+    float expect = 3.14;
     // allocate a block
     auto block = alloc(1024*1024);
-    // and use it
-    auto value = read(block);
+    // write a value
+    write(block, expect);
+    // and read it
+    auto got = read(block);
 
     // show me
     channel
-        << "read: " << value
+        << "wrote: " << expect
+        << "read: " << got
         << pyre::journal::endl(__HERE__);
+
+    // check
+    if (expect != got) {
+        // make a channel
+        pyre::journal::error_t error("ampcor.cuda.cudaheap");
+        // complain
+        error
+            << "mismatch: wrote: " << expect << ", read: " << got
+            << pyre::journal::endl(__HERE__);
+    }
 
     // let it go out of scope
     return;
