2b156f1cd0d84e6e6a4eead3d436d155468a8cc8
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-12-01 14:14:27 -0800

    ampcor::cuda: the {deramp} kernel can now accumulate the range and azimuth phases using an updated reduction algorithm; i also left behind a {lambda} version of the reducer that i'd like to deploy after some more testing

--- lib/ampcor_cuda/correlators/deramp.cu
+++ lib/ampcor_cuda/correlators/deramp.cu
@@ -15,16 +15,20 @@
 // cuda
 #include <cuda_runtime.h>
 #include <cooperative_groups.h>
+#include <thrust/complex.h>
 // pyre
 #include <pyre/journal.h>
 // local declarations
 #include "kernels.h"
 
+// type alias
+using complex_t = thrust::complex<float>;
 
 // helpers
+template <std::size_t T, typename complexT = complex_t>
 __global__
 static void
-_deramp(cuFloatComplex * arena,
+_deramp(complexT * arena,
         std::size_t arenaRows, std::size_t arenaCols, std::size_t tileRows, std::size_t tileCols);
 
 
@@ -38,30 +42,80 @@ deramp(std::complex<float> * arena,
     // make a channel
     pyre::journal::info_t channel("ampcor.cuda.deramp");
 
-    // we need enough workers to cover the largest tile rank
-    auto workers = std::max(tileRows, tileCols);
-    // round the number of threads up to the nearest full warp
-    auto T = 32 * (workers / 32 + (workers % 32 ? 1 : 0));
-    // the number of block
-    auto B = pairs;
-    // the amount of shared memory to set aside; we do warp shuffling, so there is a low bound
-    auto S = std::max(64ul, T) * sizeof(float);
+    // each thread handles a row and a column to get the horizontal and vertical accumulated
+    // phase, then we do a reduction; so we need enough workers to cover the largest tile rank
+    const auto w = std::max(tileRows, tileCols);
+    // the number of blocks
+    const auto B = pairs;
+    // each worker stores two complex numbers: the horizontal and vertical phase accumulator
+    const auto s = 2 * sizeof(complex_t);
 
     // it's not 1980 any more
     auto pl = pairs == 1 ? "" : "s";
     // show me
     channel
         << pyre::journal::at(__HERE__)
-        << "launching " << B << " block" << pl <<" of " << T
+        << "launching " << B << " block" << pl <<" of no less than " << w
         << " threads each to deramp " << pairs << " tile" << pl <<" of ("
         << tileRows << "x" << tileCols << ") pixels"
         << pyre::journal::endl;
 
-    // REDUCTION TEMPLATE EXPANSION BASED ON T
-    // launch the SAT kernel
-    _deramp <<<B,T,S>>> (reinterpret_cast<cuFloatComplex *>(arena),
-                         arenaRows, arenaCols, tileRows, tileCols);
+    // convert the arena
+    auto carena = reinterpret_cast<thrust::complex<float> *>(arena);
 
+    // deploy with warp count known at compile time
+    if (w <= 32) {
+        // set the number of threads
+        const int T = 32;
+        // the amount of shared memory to set aside; we do warp shuffling, so there is a low
+        // bound of 64 slots
+        const int S = 64 * s;
+        // tell me
+        channel << "deploying " << T << " workers";
+        // launch
+        _deramp<T> <<<B,T,S>>> (carena, arenaRows, arenaCols, tileRows, tileCols);
+    } else if (w <= 64) {
+        // set the number of threads
+        const int T = 64;
+        // the amount of shared memory to set aside
+        const int S = T * s;
+        // tell me
+        channel << "deploying " << T << " workers";
+        // launch
+        _deramp<T> <<<B,T,S>>> (carena, arenaRows, arenaCols, tileRows, tileCols);
+    } else if (w <= 128) {
+        // set the number of threads
+        const int T = 128;
+        // the amount of shared memory to set aside; we do warp shuffling, so there is a low bound;
+        const int S = T * s;
+        // tell me
+        channel << "deploying " << T << " workers";
+        // launch
+        _deramp<T> <<<B,T,S>>> (carena, arenaRows, arenaCols, tileRows, tileCols);
+    } else if (w <= 256) {
+        // set the number of threads
+        const int T = 256;
+        // the amount of shared memory to set aside; we do warp shuffling, so there is a low bound;
+        const int S = T * s;
+        // tell me
+        channel << "deploying " << T << " workers";
+        // launch
+        _deramp<T> <<<B,T,S>>> (carena, arenaRows, arenaCols, tileRows, tileCols);
+    } else if (w <= 512) {
+        // set the number of threads
+        const int T = 512;
+        // the amount of shared memory to set aside; we do warp shuffling, so there is a low bound;
+        const int S = T * s;
+        // tell me
+        channel << "deploying " << T << " workers";
+        // launch
+        _deramp<T> <<<B,T,S>>> (carena, arenaRows, arenaCols, tileRows, tileCols);
+    } else {
+        // complain
+        throw std::runtime_error("while in {deramp}: cannot handle tiles of this shape");
+    }
+    // flush
+    channel << pyre::journal::endl;
 
     // check whether all went well
     auto launchStatus = cudaGetLastError();
@@ -104,26 +158,171 @@ deramp(std::complex<float> * arena,
 
 
 // implementation
+template <std::size_t T, typename complexT>
 __global__
 static void
-_deramp(cuFloatComplex * arena,
+_deramp(complexT * arena,
         std::size_t arenaRows, std::size_t arenaCols, std::size_t tileRows, std::size_t tileCols)
 {
     // build the workload descriptors
     // global
-    // std::size_t B = gridDim.x;      // number of blocks
-    // std::size_t T = blockDim.x;        // number of threads per block
-    // auto W = B*T;                   // total number of workers
+    // auto B = gridDim.x;      // number of blocks
+    // auto T = blockDim.x;     // number of threads per block; here, a template parameter
+    // auto W = B*T;            // total number of workers
     // local
-    std::size_t b = blockIdx.x;        // my block id
-    std::size_t t = threadIdx.x;       // my thread id
-    // auto w = b*T + t;               // my worker id
+    auto b = blockIdx.x;        // my block id
+    auto t = threadIdx.x;       // my thread id
+    // auto w = b*T + t;        // my worker id
 
     // get access to my shared memory
-    extern __shared__ float scratch[];
+    extern __shared__ complexT scratch[];
+    // get a handle to this thread block group
+    cooperative_groups::thread_block cta = cooperative_groups::this_thread_block();
 
     // find the beginning of my tile by skipping the ones done by other blocks
     auto tile = arena + b * (arenaRows*arenaCols);
+
+    // initialize the phase accumulator along rows
+    complexT phaseHorz { 0, 0 };
+    // and along columns
+    complexT phaseVert { 0, 0 };
+
+    // if i have been assigned a row
+    if (t < tileRows) {
+        // first element of my row
+        auto row = tile + t*arenaCols;
+        // run across the row; don't forget to exclude the last element in this row since we
+        // look to the right as we accumulate
+        for (auto idx = 0; idx < tileCols-1; ++idx) {
+            // collect
+            phaseHorz += row[idx] * thrust::conj(row[idx+1]);
+        }
+    }
+
+    // if I have been assigned a column
+    if (t < tileCols) {
+        // first element of my column
+        auto col = tile + t;
+        // run down my column; don't forget to exclude the last element in this column since we
+        // look down as we accumulate
+        for (auto idx = 0; idx < tileRows - 1; ++idx) {
+            // collect
+            phaseVert += col[idx] * thrust::conj(col[idx+arenaCols]);
+        }
+    }
+
+    // now, we all save our partial results to shared memory; this includes the guys that have
+    // been idling during either or both of the traversals; each thread is assigned two slots:
+    // one for the horizontal phase accumulator and one for the vertical
+    // my slot is at
+    auto slot = scratch + 2*t;
+    // write the horizontal phase
+    slot[0] = phaseHorz;
+    // and the vertical phase
+    slot[1] = phaseVert;
+    // barrier
+    cta.sync();
+
+    // reduction time; the following section is a loop unrolled by hand, with compile-time
+    // checks to remove the unused blocks; the general strategy is to have the low half of the
+    // remaining block read the accumulators of the upper, update its own, and store in shared
+    // memory; we then half the block size and try again, until we hit warp size, then we
+    // shuffle
+
+#ifdef MGA
+    // here is a lambda version of the reducer; debug...
+    auto reduce = [&] (int halfBlock) {
+        // my sibling from the upper half of the block stored its accumulators here
+        auto sibling = scratch + 2*(t+halfBlock);
+        // update my horizontal phase
+        phaseHorz += sibling[0];
+        // and my vertical phase
+        phaseVert += sibling[1];
+        // record my values
+        slot[0] = phaseHorz;
+        slot[1] = phaseVert;
+        // all done
+        return;
+    };
+#endif
+
+    // for 512 threads per block
+    if (T >= 512 && t < 256) {
+        // my sibling from the upper half of the block stored its accumulators here
+        auto sibling = scratch + 2*(t+256);
+        // update my horizontal phase
+        phaseHorz += sibling[0];
+        // and my vertical phase
+        phaseVert += sibling[1];
+        // record my values
+        slot[0] = phaseHorz;
+        slot[1] = phaseVert;
+    }
+    // barrier
+    cta.sync();
+
+    // for 256 threads per block
+    if (T >= 256 && t < 128) {
+        // my sibling from the upper half of the block stored its accumulators here
+        auto sibling = scratch + 2*(t+128);
+        // update my horizontal phase
+        phaseHorz += sibling[0];
+        // and my vertical phase
+        phaseVert += sibling[1];
+        // record my values
+        slot[0] = phaseHorz;
+        slot[1] = phaseVert;
+    }
+    // barrier
+    cta.sync();
+
+    // for 128 threads per block
+    if (T >= 128 && t < 64) {
+        // my sibling from the upper half of the block stored its accumulators here
+        auto sibling = scratch + 2*(t+64);
+        // update my accumulators
+        phaseHorz += sibling[0];
+        phaseVert += sibling[1];
+        // record my values
+        slot[0] = phaseHorz;
+        slot[1] = phaseVert;
+    }
+    // barrier
+    cta.sync();
+
+    // ok; at this point, the half block is a warp and we can shuffle
+    if (t < 32) {
+        // if we need to pull values from the upper half of a block
+        if (T >= 64) {
+            // my sibling from the upper half of the block stored its accumulators here
+            auto sibling = scratch + 2*(t+64);
+            // update my accumulators, but no need to bother updating shared memory; we are
+            // done with it
+            phaseHorz += sibling[0];
+            phaseVert += sibling[1];
+        }
+        // get a handle to the active thread group
+        cooperative_groups::coalesced_group active = cooperative_groups::coalesced_threads();
+        // the threads with power-of-2 ids
+        for (auto offset = 16; offset > 0; offset >>= 1) {
+            // reduce using shuffle
+            phaseHorz += active.shfl_down(phaseHorz, offset);
+            phaseVert += active.shfl_down(phaseVert, offset);
+        }
+    }
+    // barrier
+    cta.sync();
+
+    // the reduced values are available only on thread 0
+    if (t == 0) {
+        // let's write them in a known location in shared memory so everybody has access
+        scratch[0] =  phaseHorz;
+        scratch[1] =  phaseVert;
+    }
+
+    // finally, we have to deramp the tile using the phase accumulators
+    //
+    //
 
     // all done
     return;
