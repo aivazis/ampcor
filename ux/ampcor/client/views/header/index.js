9b8cd6598502f45ee5f3c1e7e5513a843cfded2d
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-08-25 15:04:34 +0300

    ux/views: fixed the tooltips of the {header} links

--- ux/ampcor/client/views/header/index.js
+++ ux/ampcor/client/views/header/index.js
@@ -21,9 +21,9 @@ const header = () => {
     const hide = () => setVisibility("hidden")
 
     // get the {nav} style and adjust the visibility of its container
-    var navStyle = {...styles.nav.box, visibility}
+    var navStyle = { ...styles.nav.box, visibility }
     // get the {kill} widget styling
-    var killStyle = {...styles.kill}
+    var killStyle = { ...styles.kill }
     // and adjust the visibility of its container as well
     killStyle.box.visibility = visibility
 
@@ -33,18 +33,18 @@ const header = () => {
     // assemble the style for the link
     const decorate = name => (
         location.startsWith('/' + name) ?
-        {...styles.nav.name, ...styles.nav.current} : styles.nav.name
+            { ...styles.nav.name, ...styles.nav.current } : styles.nav.name
     )
 
     // the table of page links
     const links = [
-        { name: "flow", titile: "configure the ampcor workflow" },
-        { name: "exp", titile: "my sandbox" },
-        { name: "slc", titile: "display the input rasters" },
-        { name: "offsets", titile: "visualize the estimated offsets" },
-        { name: "plan", titile: "show the correlation plan" },
-        { name: "gamma", titile: "visualize the correlaton surface" },
-        ]
+        { name: "flow", title: "configure the ampcor workflow" },
+        { name: "exp", title: "my sandbox" },
+        { name: "slc", title: "display the input rasters" },
+        { name: "offsets", title: "visualize the estimated offsets" },
+        { name: "plan", title: "show the correlation plan" },
+        { name: "gamma", title: "visualize the correlation surface" },
+    ]
 
     // build the component and return it
     return (
@@ -57,27 +57,28 @@ const header = () => {
 
             {/* the menu */}
             <nav style={navStyle}>
-                {links.map( (link, index) => {
-                     // link styling
-                     const linkStyle = (
-                         (index === links.length-1)
-                         ? {...styles.nav.link, ...styles.nav.last}
-                         : styles.nav.link )
-                     // entry styling
-                     const entryStyle = decorate(link.name)
-                     // render
-                     return (
-                         <Link key={link.name} to={link.name} style={linkStyle}>
-                             <span style={entryStyle} title={link.title}>
-                                 {link.name}
-                             </span>
-                         </Link>
-                     )}
-                 )}
+                {links.map((link, index) => {
+                    // link styling
+                    const linkStyle = (
+                        (index === links.length - 1)
+                            ? { ...styles.nav.link, ...styles.nav.last }
+                            : styles.nav.link)
+                    // entry styling
+                    const entryStyle = decorate(link.name)
+                    // render
+                    return (
+                        <Link key={link.name} to={link.name} style={linkStyle}>
+                            <span style={entryStyle} title={link.title}>
+                                {link.name}
+                            </span>
+                        </Link>
+                    )
+                }
+                )}
             </nav>
 
             {/* the kill button */}
-            <Kill style={killStyle} onKill={hide} transform="scale(0.1 0.1)"/>
+            <Kill style={killStyle} onKill={hide} transform="scale(0.1 0.1)" />
 
         </header>
     )
