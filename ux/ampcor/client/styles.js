fd4e4fa2d08f8708b7d3bf0bfccffef2eb541191
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-11-22 09:19:32 -0800

    ux/views: the main page styling is now the responsibility of {views}

--- ux/ampcor/client/styles.js
+++ ux/ampcor/client/styles.js
@@ -11,19 +11,6 @@ import { theme } from '~/palette'
 
 // styling for the page container
 export default {
-    // the top level flex container
-    layout : {
-        // placement
-        width: "100%",
-        height: "100%",
-
-        // overall styling
-        backgroundColor: theme.page.background,
-
-        // for my children
-        display: "flex",
-        flexDirection: "column",
-    },
 }
 
 
